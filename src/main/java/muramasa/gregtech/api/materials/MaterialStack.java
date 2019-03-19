337fb2b6ef22536e2b2f8fab2158cadb61a0c5cb
Muramasa <haydenkilloh@gmail.com>
2019-03-19 20:21:53 +0000

    fix blast furnace duration for dusts

--- src/main/java/muramasa/gregtech/api/materials/MaterialStack.java
+++ src/main/java/muramasa/gregtech/api/materials/MaterialStack.java
@@ -2,19 +2,11 @@ package muramasa.gregtech.api.materials;
 
 public class MaterialStack {
 
-    private Material material;
-    private int size;
+    public Material m;
+    public int s;
 
     public MaterialStack(Material material, int size) {
-        this.material = material;
-        this.size = size;
-    }
-
-    public Material get() {
-        return material;
-    }
-
-    public int size() {
-        return size;
+        m = material;
+        s = size;
     }
 }
