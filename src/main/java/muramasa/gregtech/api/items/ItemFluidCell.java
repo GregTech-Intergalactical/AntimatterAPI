713a2f7b5f045e186b3f1c38715647fa4d156982
Muramasa <haydenkilloh@gmail.com>
2019-03-22 10:28:44 +0000

    implement dynamic fluid cell

--- src/main/java/muramasa/gregtech/api/items/ItemFluidCell.java
+++ src/main/java/muramasa/gregtech/api/items/ItemFluidCell.java
@@ -1,4 +1,71 @@
 package muramasa.gregtech.api.items;
 
-public class ItemFluidCell {
+import muramasa.gregtech.api.data.Materials;
+import muramasa.gregtech.client.render.GTModelLoader;
+import muramasa.gregtech.client.render.models.ModelFluidCell;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
+import net.minecraftforge.fluids.capability.IFluidHandlerItem;
+import net.minecraftforge.fluids.capability.templates.FluidHandlerItemStackSimple;
+
+import javax.annotation.Nullable;
+import java.util.List;
+
+public class ItemFluidCell extends Item {
+
+    private static int CAPACITY = 1000;
+
+    public ItemFluidCell() {
+        setUnlocalizedName("fluid_cell");
+        setRegistryName("fluid_cell");
+        setMaxStackSize(1);
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+        FluidStack fluid = getContents(stack);
+        if (fluid != null) {
+            tooltip.add(fluid.getLocalizedName() + " - " + fluid.amount);
+        }
+    }
+
+    @Override
+    public EnumActionResult onItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+        IFluidHandlerItem handler = player.getHeldItem(hand).getCapability(CapabilityFluidHandler.FLUID_HANDLER_ITEM_CAPABILITY, null);
+        if (handler != null) {
+            System.out.println("filling");
+            handler.fill(Materials.Titaniumtetrachloride.getLiquid(1000), true);
+        }
+        return EnumActionResult.SUCCESS;
+    }
+
+    @Nullable
+    public static FluidStack getContents(ItemStack stack) {
+        IFluidHandlerItem handler = stack.getCapability(CapabilityFluidHandler.FLUID_HANDLER_ITEM_CAPABILITY, null);
+        return handler.getTankProperties()[0].getContents();
+    }
+
+    @Nullable
+    @Override
+    public ICapabilityProvider initCapabilities(ItemStack stack, @Nullable NBTTagCompound nbt) {
+        return new FluidHandlerItemStackSimple(stack, CAPACITY);
+    }
+
+    public void initModel() {
+        ModelLoader.setCustomModelResourceLocation(this, 0, new ModelResourceLocation(getRegistryName(), "inventory"));
+        GTModelLoader.register(getRegistryName().getResourcePath(), new ModelFluidCell());
+    }
 }
---
+++ src/main/java/muramasa/gregtech/client/render/models/ModelFluidCell.java
@@ -0,0 +1,62 @@
+package muramasa.gregtech.client.render.models;
+
+import com.google.common.collect.ImmutableList;
+import muramasa.gregtech.Ref;
+import muramasa.gregtech.client.render.bakedmodels.BakedBase;
+import muramasa.gregtech.client.render.bakedmodels.BakedFluidCell;
+import net.minecraft.client.renderer.block.model.BakedQuad;
+import net.minecraft.client.renderer.block.model.IBakedModel;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.renderer.vertex.VertexFormat;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.IModel;
+import net.minecraftforge.common.model.IModelState;
+import net.minecraftforge.fluids.Fluid;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Function;
+
+public class ModelFluidCell implements IModel {
+
+    private static ResourceLocation BASE = new ResourceLocation(Ref.MODID, "items/fluid_cell");
+    private static IBakedModel BAKED_BASE, BAKED_OVERLAY;
+
+    private Fluid fluid;
+
+    public ModelFluidCell() {
+        //Default Constructor
+    }
+
+    public ModelFluidCell(Fluid fluid) {
+        this.fluid = fluid;
+    }
+
+    @Override
+    public IBakedModel bake(IModelState state, VertexFormat format, Function<ResourceLocation, TextureAtlasSprite> getter) {
+        if (BAKED_BASE == null) BAKED_BASE = ModelBase.load(Ref.MODID, "fluid_cell_model").bake(state, format, getter);
+        if (BAKED_OVERLAY == null) BAKED_OVERLAY = ModelBase.load(Ref.MODID, "fluid_cell_overlay_model").bake(state, format, getter);
+
+        ImmutableList.Builder<BakedQuad> builder = ImmutableList.builder();
+
+        if (fluid != null) {
+            TextureAtlasSprite sprite = getter.apply(fluid.getStill());
+            if (sprite != null) {
+                List<BakedQuad> quads = BAKED_OVERLAY.getQuads(null, null, 0);
+                quads = BakedBase.tex(quads, fluid.getColor(), sprite);
+                builder.addAll(quads);
+            }
+        }
+        builder.addAll(BAKED_BASE.getQuads(null, null, 0));
+
+        return new BakedFluidCell(builder.build(), this, getter.apply(BASE), format);
+    }
+
+    @Override
+    public Collection<ResourceLocation> getTextures() {
+        ArrayList<ResourceLocation> locs = new ArrayList<>();
+        locs.add(BASE);
+        return locs;
+    }
+}
