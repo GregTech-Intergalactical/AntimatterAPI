2b7adeac776405652476334f21ccfdf607f83798
Muramasa <haydenkilloh@gmail.com>
2019-04-07 05:01:01 +0100

    change item pipe types and wire item rendering

--- src/main/java/muramasa/gtu/Ref.java
+++ src/main/java/muramasa/gtu/Ref.java
@@ -17,7 +17,7 @@ public class Ref {
     /** Mod Data **/
     public static final String MODID = "gtu";
     public static final String NAME = "GregTech";
-    public static final String VERSION = "1.00.01";
+    public static final String VERSION = "0.00.01";
     public static final String CLIENT = "muramasa.gtu.proxy.ClientProxy";
     public static final String SERVER = "muramasa.gtu.proxy.ServerProxy";
 
