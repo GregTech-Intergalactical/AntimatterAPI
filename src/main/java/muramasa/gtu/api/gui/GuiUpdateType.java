51c8a4f8abd5f832800597debcd610d882cd07de
Muramasa <haydenkilloh@gmail.com>
2019-04-13 04:32:52 +0100

    throttle client gui updates

--- src/main/java/muramasa/gtu/api/gui/GuiUpdateType.java
+++ src/main/java/muramasa/gtu/api/gui/GuiUpdateType.java
@@ -2,6 +2,16 @@ package muramasa.gtu.api.gui;
 
 public enum GuiUpdateType {
 
-    PROGRESS,
-    MACHINE_STATE
+    PROGRESS(1.0f),
+    MACHINE_STATE(1.0f);
+
+    private float updateThreshold;
+
+    GuiUpdateType(float updateThreshold) {
+        this.updateThreshold = updateThreshold;
+    }
+
+    public float getUpdateThreshold() {
+        return updateThreshold;
+    }
 }
