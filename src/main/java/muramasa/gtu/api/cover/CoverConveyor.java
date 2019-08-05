669375da57a15feeac38fd319b15a95b69168ff9
Muramasa <haydenkilloh@gmail.com>
2019-08-05 03:12:56 +0100

    implement conveyor cover

--- src/main/java/muramasa/gtu/api/cover/CoverConveyor.java
+++ src/main/java/muramasa/gtu/api/cover/CoverConveyor.java
@@ -1,8 +1,12 @@
 package muramasa.gtu.api.cover;
 
 import muramasa.gtu.api.machines.Tier;
+import muramasa.gtu.api.util.Utils;
 import muramasa.gtu.common.Data;
 import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraftforge.items.CapabilityItemHandler;
 
 public class CoverConveyor extends Cover {
 
@@ -19,7 +23,10 @@ public class CoverConveyor extends Cover {
 
     @Override
     public ItemStack getDroppedStack() {
-        switch (tier.getId()) { //TODO maybe a better way to do this? but for now, it works.
+        //TODO maybe a better way to do this? but for now, it works.
+        //TODO coverStacks should probably be tier sensitive? this would mean
+        //TODO all covers would need a tier member
+        switch (tier.getId()) {
             case "lv": return Data.ConveyorLV.get(1);
             case "mv": return Data.ConveyorMV.get(1);
             case "hv": return Data.ConveyorHV.get(1);
@@ -27,5 +34,14 @@ public class CoverConveyor extends Cover {
             case "iv": return Data.ConveyorIV.get(1);
             default: return Data.ConveyorLV.get(1);
         }
+    }
+
+    @Override
+    public void onUpdate(TileEntity tile, EnumFacing side) {
+        if (tile.getWorld().getTotalWorldTime() % 20 != 0) return;
+        TileEntity adjTile = tile.getWorld().getTileEntity(tile.getPos().offset(side));
+        if (adjTile == null) return;
+        if (!tile.hasCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, side.getOpposite()) || !adjTile.hasCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, side.getOpposite())) return;
+        Utils.transferItems(tile.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, side.getOpposite()), adjTile.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, side.getOpposite()));
     }
 }
