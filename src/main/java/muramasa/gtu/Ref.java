20f3e4a41f6332f0958a95e06fa1988f2f8a7af9
Muramasa <haydenkilloh@gmail.com>
2019-08-18 13:47:02 +0100

    load after CT, need to validate addons do not run into race conditions

--- src/main/java/muramasa/gtu/Ref.java
+++ src/main/java/muramasa/gtu/Ref.java
@@ -18,7 +18,7 @@ public class Ref {
     public static final String MODID = "gtu";
     public static final String NAME = "GregTech";
     public static final String VERSION = "0.1.0";
-    public static final String DEPENDS = "before:crafttweaker; after:undergroundbiomes";
+    public static final String DEPENDS = "after:crafttweaker; after:undergroundbiomes";
     public static final String CLIENT = "muramasa.gtu.proxy.ClientProxy";
     public static final String SERVER = "muramasa.gtu.proxy.ServerProxy";
 
