1447fa7d3ee0b1e1562ade98376740fdcd40e0fd
Muramasa <haydenkilloh@gmail.com>
2019-08-22 17:07:40 +0100

    cleanup some worldgen object handling

--- src/main/java/muramasa/gtu/api/worldgen/GregTechWorldGenerator.java
+++ src/main/java/muramasa/gtu/api/worldgen/GregTechWorldGenerator.java
@@ -3,23 +3,16 @@ package muramasa.gtu.api.worldgen;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.reflect.TypeToken;
 import com.google.gson.internal.LinkedTreeMap;
-import exterminatorjeff.undergroundbiomes.api.event.UBForceReProcessEvent;
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 import muramasa.gtu.GregTech;
 import muramasa.gtu.Ref;
-import muramasa.gtu.api.data.Materials;
 import muramasa.gtu.api.util.XSTR;
-import muramasa.gtu.loaders.WorldGenLoader;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.IChunkGenerator;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.terraingen.PopulateChunkEvent;
 import net.minecraftforge.fml.common.IWorldGenerator;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.eventhandler.EventPriority;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 import java.io.BufferedWriter;
@@ -42,6 +35,7 @@ public class GregTechWorldGenerator implements IWorldGenerator {
     private static HashMap<String, HashMap<String, Object>> DEFAULT_DATA = new HashMap<>();
     private static ImmutableMap<String, Class<?>> TYPE = ImmutableMap.of("vein", WorldGenOreVein.class, "small", WorldGenOreSmall.class, "stone", WorldGenStone.class);
 
+    private static Int2ObjectOpenHashMap<List<WorldGenBase>> BASE = new Int2ObjectOpenHashMap<>();
     private static Int2ObjectOpenHashMap<List<WorldGenOreVein>> LAYER = new Int2ObjectOpenHashMap<>();
     private static Int2ObjectOpenHashMap<List<WorldGenOreSmall>> SMALL = new Int2ObjectOpenHashMap<>();
     private static Int2ObjectOpenHashMap<List<WorldGenStone>> STONE = new Int2ObjectOpenHashMap<>();
@@ -51,6 +45,7 @@ public class GregTechWorldGenerator implements IWorldGenerator {
         REGISTRY.put("vein", new HashMap<>());
         REGISTRY.put("small", new HashMap<>());
         REGISTRY.put("stone", new HashMap<>());
+        REGISTRY.put("base", new HashMap<>());
     }
 
     public GregTechWorldGenerator() {
@@ -59,8 +54,9 @@ public class GregTechWorldGenerator implements IWorldGenerator {
 
     public static void register(WorldGenBase worldGen) {
         if (worldGen instanceof WorldGenOreVein) REGISTRY.get("vein").put(worldGen.getId(), worldGen);
-        if (worldGen instanceof WorldGenOreSmall) REGISTRY.get("small").put(worldGen.getId(), worldGen);
-        if (worldGen instanceof WorldGenStone) REGISTRY.get("stone").put(worldGen.getId(), worldGen);
+        else if (worldGen instanceof WorldGenOreSmall) REGISTRY.get("small").put(worldGen.getId(), worldGen);
+        else if (worldGen instanceof WorldGenStone) REGISTRY.get("stone").put(worldGen.getId(), worldGen);
+        else REGISTRY.get("base").put(worldGen.getId(), worldGen);
     }
 
     public static void init() {
@@ -125,6 +121,9 @@ public class GregTechWorldGenerator implements IWorldGenerator {
             REGISTRY.get("stone").values().stream().filter(WorldGenBase::isEnabled).forEach(w -> w.getDimensions().forEach(d -> {
                 STONE.computeIfAbsent(d, k -> new ArrayList<>()).add((WorldGenStone) w.build());
             }));
+            REGISTRY.get("base").values().stream().filter(WorldGenBase::isEnabled).forEach(w -> w.getDimensions().forEach(d -> {
+                BASE.computeIfAbsent(d, k -> new ArrayList<>()).add(w.build());
+            }));
             GregTech.LOGGER.info("GregTechWorldGenerator: Finished data rebuild!");
         } catch (Exception e) {
             e.printStackTrace();
@@ -144,74 +143,35 @@ public class GregTechWorldGenerator implements IWorldGenerator {
         return STONE.get(dimension);
     }
 
-    @SubscribeEvent(priority = EventPriority.LOWEST)
-    public static void onPopulateChunkPost(PopulateChunkEvent.Post e) {
-        MinecraftForge.EVENT_BUS.post(new UBForceReProcessEvent(e.getGenerator(), e.getWorld(), e.getRand(), e.getChunkX(), e.getChunkZ(), false));
-        handleOres(e.getRand(), e.getChunkX(), e.getChunkZ(), e.getWorld());
-    }
-
     @Override
     public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator generator, IChunkProvider provider) {
         try {
             XSTR rand = new XSTR(Math.abs(random.nextInt()) + 1);
-
-            WorldGenLoader.RUBBER_TREE_GEN.generate(world, rand, chunkX * 16, chunkZ * 16, new BlockPos.MutableBlockPos(), null, generator, provider);
-
-            int rockAmount = 4;
-            int passedX = chunkX * 16;
-            int passedZ = chunkZ * 16;
-            int j = Math.max(1, rockAmount + rand.nextInt(rockAmount));
             BlockPos.MutableBlockPos pos = new BlockPos.MutableBlockPos();
-            for (int i = 0; i < j; i++) {
-                pos.setPos(passedX + 8 + rand.nextInt(16), 0, passedZ + 8 + rand.nextInt(16));
-                pos.setY(world.getHeight(pos.getX(), pos.getZ()) - 1);
-                WorldGenHelper.setRock(world, pos, Materials.NULL);
+            List<WorldGenBase> worldGenObjects = BASE.get(world.provider.getDimension());
+            if (worldGenObjects != null && worldGenObjects.size() > 0) {
+                worldGenObjects.forEach(o -> o.generate(world, rand, chunkX * 16, chunkZ * 16, pos, null, generator, provider));
             }
 
-            //Generate Stones and Small Ores
-            List<WorldGenStone> stones = STONE.get(world.provider.getDimension());
-            if (stones != null && stones.size() > 0) {
-                for (WorldGenStone stone : stones) {
-                    stone.generate(world, rand, chunkX * 16, chunkZ * 16, new BlockPos.MutableBlockPos(), null, generator, provider);
-                }
-            }
+            if (LAYER.size() > 0) {
+                // Determine bounding box on how far out to check for ore veins affecting this chunk
+                int westX = chunkX - (Ref.ORE_VEIN_MAX_SIZE / 16);
+                int eastX = chunkX + (Ref.ORE_VEIN_MAX_SIZE / 16 + 1); // Need to add 1 since it is compared using a <
+                int northZ = chunkZ - (Ref.ORE_VEIN_MAX_SIZE / 16);
+                int southZ = chunkZ + (Ref.ORE_VEIN_MAX_SIZE / 16 + 1);
 
-            List<WorldGenOreSmall> smalls = SMALL.get(world.provider.getDimension());
-            if (smalls != null && smalls.size() > 0) {
-                for (WorldGenOreSmall small : smalls) {
-                    small.generate(world, rand, chunkX * 16, chunkZ * 16, new BlockPos.MutableBlockPos(), null, generator, provider);
+                // Search for oreVein seeds and add to the list;
+                for (int x = westX; x < eastX; x++) {
+                    for (int z = northZ; z < southZ; z++) {
+                        if (((Math.abs(x) % 3) == 1) && ((Math.abs(z) % 3) == 1)) { //Determine if this X/Z is an oreVein seed
+                            WorldGenOreVein.generate(world, chunkX, chunkZ, x, z, pos, null);
+                        }
+                    }
                 }
+                //if (Ref.debugWorldGen) GregTech.LOGGER.info("Oregen took " + (oreGenTime - leftOverTime) + " Leftover gen took " + (leftOverTime - startTime) + " Worldgen took " + duration + " ns");
             }
         } catch (Exception e) {
             e.printStackTrace();
-        }
-    }
-
-    public static void handleOres(Random random, int chunkX, int chunkZ, World world) {
-        XSTR rand = new XSTR(Math.abs(random.nextInt()) + 1);
-        BlockPos.MutableBlockPos pos = new BlockPos.MutableBlockPos();
-
-        if (LAYER.size() > 0) {
-            // Determine bounding box on how far out to check for ore veins affecting this chunk
-            int westX = chunkX - (Ref.ORE_VEIN_MAX_SIZE / 16);
-            int eastX = chunkX + (Ref.ORE_VEIN_MAX_SIZE / 16 + 1); // Need to add 1 since it is compared using a <
-            int northZ = chunkZ - (Ref.ORE_VEIN_MAX_SIZE / 16);
-            int southZ = chunkZ + (Ref.ORE_VEIN_MAX_SIZE / 16 + 1);
-
-            // Search for oreVein seeds and add to the list;
-            for (int x = westX; x < eastX; x++) {
-                for (int z = northZ; z < southZ; z++) {
-                    if (((Math.abs(x) % 3) == 1) && ((Math.abs(z) % 3) == 1)) { //Determine if this X/Z is an oreVein seed
-                        WorldGenOreVein.generate(world, chunkX, chunkZ, x, z, pos, null);
-                    }
-                }
-            }
-
-            if (world.provider.getDimension() == Ref.END || world.provider.getDimension() == Ref.ASTEROIDS) {
-                WorldGenLoader.ASTEROID_GEN.generate(world, rand, chunkX, chunkZ, pos, null, null, null);
-            }
-
-            //if (Ref.debugWorldGen) GregTech.LOGGER.info("Oregen took " + (oreGenTime - leftOverTime) + " Leftover gen took " + (leftOverTime - startTime) + " Worldgen took " + duration + " ns");
         }
     }
 }
