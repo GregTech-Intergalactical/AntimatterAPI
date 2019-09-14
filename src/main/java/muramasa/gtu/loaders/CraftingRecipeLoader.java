7c96b39bd4374c969e5538ea7b179355ec18353e
Ethan Mahmood <ethanmahmood@gmail.com>
2019-09-15 00:25:51 +0100

    Added main machine casing recipes - Overriden asItemStack in BlockBaked (needs verifying)

--- src/main/java/muramasa/gtu/loaders/CraftingRecipeLoader.java
+++ src/main/java/muramasa/gtu/loaders/CraftingRecipeLoader.java
@@ -1,41 +1,52 @@
 package muramasa.gtu.loaders;
 
-import muramasa.gtu.api.data.Materials;
 import muramasa.gtu.api.recipe.RecipeHelper;
 import muramasa.gtu.api.tools.ToolType;
-import muramasa.gtu.common.Data;
+import muramasa.gtu.api.util.Utils;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 
+import static muramasa.gtu.api.data.Materials.*;
+import static muramasa.gtu.common.Data.*;
+
 public class CraftingRecipeLoader {
 
     public static void init() {
-        
-        //Machines
-        
-        ItemStack rockFlint = Materials.Flint.getRock(1);
+        ItemStack rockFlint = Flint.getRock(1);        
+        //Casings        
+        RecipeHelper.addShaped("casing_ulv", CASING_ULV.asItemStack(), "PPP", "PwP", "PPP", 'P', WroughtIron.getPlate(1));
+        RecipeHelper.addShaped("casing_lv", CASING_LV.asItemStack(), "PPP", "PwP", "PPP", 'P', Steel.getPlate(1));
+        RecipeHelper.addShaped("casing_mv", CASING_MV.asItemStack(), "PPP", "PwP", "PPP", 'P', Aluminium.getPlate(1));
+        RecipeHelper.addShaped("casing_hv", CASING_HV.asItemStack(), "PPP", "PwP", "PPP", 'P', StainlessSteel.getPlate(1));
+        RecipeHelper.addShaped("casing_ev", CASING_EV.asItemStack(), "PPP", "PwP", "PPP", 'P', Titanium.getPlate(1));
+        RecipeHelper.addShaped("casing_iv", CASING_IV.asItemStack(), "PPP", "PwP", "PPP", 'P', TungstenSteel.getPlate(1));
+        RecipeHelper.addShaped("casing_luv", CASING_LUV.asItemStack(), "PPP", "PwP", "PPP", 'P', Chrome.getPlate(1));
+        RecipeHelper.addShaped("casing_zpm", CASING_ZPM.asItemStack(), "PPP", "PwP", "PPP", 'P', Iridium.getPlate(1));
+        RecipeHelper.addShaped("casing_uv", CASING_UV.asItemStack(), "PPP", "PwP", "PPP", 'P', Osmium.getPlate(1));
+        RecipeHelper.addShaped("casing_max", CASING_MAX.asItemStack(), "PPP", "PwP", "PPP", 'P', Neutronium.getPlate(1));
+  
         RecipeHelper.addShapeless("rock_to_flint", new ItemStack(Items.FLINT), rockFlint, rockFlint, ToolType.MORTAR.getOreDict());
         
-        RecipeHelper.addShaped("mortar_flint", ToolType.MORTAR.get(Materials.Flint), " F ", "SFS", "SSS", 'F', Items.FLINT, 'S', "stone");
+        RecipeHelper.addShaped("mortar_flint", ToolType.MORTAR.get(Flint), " F ", "SFS", "SSS", 'F', Items.FLINT, 'S', "stone");
 
-        RecipeHelper.addShaped("hopper", new ItemStack(Blocks.HOPPER), "IxI", "ICI", " I ", 'I', Materials.Vibranium.getIngot(1), 'C', new ItemStack(Blocks.CHEST));
-        RecipeHelper.addShapeless("scanner", Data.DebugScanner.get(1), Materials.Vibranium.getIngot(1), Materials.Iridium.getIngot(1));
+        RecipeHelper.addShaped("hopper", new ItemStack(Blocks.HOPPER), "IxI", "ICI", " I ", 'I', Vibranium.getIngot(1), 'C', new ItemStack(Blocks.CHEST));
+        RecipeHelper.addShapeless("scanner", DebugScanner.get(1), Vibranium.getIngot(1), Iridium.getIngot(1));
         
-        RecipeHelper.addShaped("gear_wood", Materials.Wood.getGear(1), "SPS", "PwP", "SPS", 'P', "plankWood", 'S', "stickWood");   
-        RecipeHelper.addShaped("gear_stone", Materials.Stone.getGear(1), "SPS", "PhP", "SPS", 'P', "cobblestone", 'S', "stone"); 
+        RecipeHelper.addShaped("gear_wood", Wood.getGear(1), "SPS", "PwP", "SPS", 'P', "plankWood", 'S', "stickWood");   
+        RecipeHelper.addShaped("gear_stone", Stone.getGear(1), "SPS", "PhP", "SPS", 'P', "cobblestone", 'S', "stone"); 
         
-        //RecipeHelper.addShapeless("paper_ring", Materials.Paper.getRing(1), "k", "X", 'X', Materials.Paper.getRing(1));
-        //RecipeHelper.addShapeless("silicone_rubber__ring", Materials.SiliconeRubber.getRing(1), "k", "X", 'X', Materials.SiliconeRubber.getRing(1));
-        RecipeHelper.addShapeless("rubber_ring", Materials.Rubber.getRing(1), ToolType.KNIFE.getOreDict(), Materials.Rubber.getPlate(1));
-        RecipeHelper.addShapeless("styrene_butadiene_rubber_ring", Materials.StyreneButadieneRubber.getRing(1), ToolType.KNIFE.getOreDict(), Materials.StyreneButadieneRubber.getPlate(1));
+        //RecipeHelper.addShapeless("paper_ring", Paper.getRing(1), "k", "X", 'X', Paper.getRing(1));
+        //RecipeHelper.addShapeless("silicone_rubber__ring", SiliconeRubber.getRing(1), "k", "X", 'X', SiliconeRubber.getRing(1));
+        RecipeHelper.addShapeless("rubber_ring", Rubber.getRing(1), ToolType.KNIFE.getOreDict(), Rubber.getPlate(1));
+        RecipeHelper.addShapeless("styrene_butadiene_rubber_ring", StyreneButadieneRubber.getRing(1), ToolType.KNIFE.getOreDict(), StyreneButadieneRubber.getPlate(1));
 
         RecipeHelper.addShaped("rubber_torch", new ItemStack(Blocks.TORCH, 4), " R ", " S ", "   ", 'R', "dropRubber", 'S', "stickWood");
-        //RecipeHelper.addShaped("sulfur_torch", new ItemStack(Blocks.TORCH, 6), " R ", " S ", "   ", 'R', Materials.Sulfur.getDust(1), 'S', "stickWood");
+        //RecipeHelper.addShaped("sulfur_torch", new ItemStack(Blocks.TORCH, 6), " R ", " S ", "   ", 'R', Sulfur.getDust(1), 'S', "stickWood");
     
         RecipeHelper.addShapeless("resin_sticky_piston", new ItemStack(Blocks.STICKY_PISTON), "dropRubber", Blocks.PISTON);
         
-        RecipeHelper.addShaped("item_filter", Data.ItemFilter.asItemStack(), "XXX", "XYX", "XXX", 'X', Materials.Zinc.getFoil(1), 'Y', Materials.Iron.getPlate(1));
-        RecipeHelper.addShapeless("magnetic_rod", Materials.IronMagnetic.getRod(1), Materials.Iron.getRod(1), Items.REDSTONE, Items.REDSTONE, Items.REDSTONE, Items.REDSTONE);
+        RecipeHelper.addShaped("item_filter", ItemFilter.asItemStack(), "XXX", "XYX", "XXX", 'X', Zinc.getFoil(1), 'Y', Iron.getPlate(1));
+        RecipeHelper.addShapeless("magnetic_rod", IronMagnetic.getRod(1), Iron.getRod(1), Items.REDSTONE, Items.REDSTONE, Items.REDSTONE, Items.REDSTONE);
     }
 }
