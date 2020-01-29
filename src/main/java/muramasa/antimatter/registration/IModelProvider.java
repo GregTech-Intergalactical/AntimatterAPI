7aa4111fbf1b18db3d570fd77ea42b0e84681ac4
Muramasa <haydenkilloh@gmail.com>
2020-01-29 22:15:34 +0000

    add ExistingFileHelperOverride

--- src/main/java/muramasa/antimatter/registration/IModelProvider.java
+++ src/main/java/muramasa/antimatter/registration/IModelProvider.java
@@ -9,9 +9,7 @@ import net.minecraftforge.common.extensions.IForgeBlock;
 public interface IModelProvider {
 
     default void onItemModelBuild(IItemProvider item, AntimatterItemModelProvider prov) {
-        if (item instanceof IForgeBlock) {
-            prov.blockItem(item);
-        }
+        if (item instanceof IForgeBlock) prov.blockItem(item);
         else if (item instanceof ITextureProvider) prov.tex(item, ((ITextureProvider) item).getTextures());
     }
 
