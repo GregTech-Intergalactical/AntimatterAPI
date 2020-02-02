58a2b6bf9e0ec1304b25c4555fa0b7e79ee17c5e
Tec <daniel112092@gmail.com>
2020-02-02 20:51:52 +0100

    Add methods to AlignmentLimits implementation

--- src/main/java/muramasa/antimatter/alignment/AlignmentLimits.java
+++ src/main/java/muramasa/antimatter/alignment/AlignmentLimits.java
@@ -7,6 +7,8 @@ import net.minecraft.util.Direction;
 
 import javax.annotation.Nonnull;
 import java.util.Arrays;
+import java.util.Optional;
+import java.util.Random;
 
 import static muramasa.antimatter.alignment.IAlignment.STATES_COUNT;
 import static muramasa.antimatter.alignment.IAlignment.getAlignmentIndex;
@@ -15,13 +17,138 @@ public class AlignmentLimits implements IAlignmentLimits {
 
     protected final boolean[] validStates=new boolean[STATES_COUNT];
 
-    private AlignmentLimits(ExtendedFacing... disabled) {
+    public AlignmentLimits() {
+        allowAll();
+    }
+
+    AlignmentLimits allowAll(){
         Arrays.fill(validStates,true);
-        if(disabled!=null){
-            for (ExtendedFacing extendedFacing : disabled) {
+        return this;
+    }
+
+    AlignmentLimits denyAll(){
+        Arrays.fill(validStates,false);
+        return this;
+    }
+
+    AlignmentLimits randomAll(@Nonnull Random random){
+        for (int i = 0; i < validStates.length; i++) {
+            validStates[i]=random.nextBoolean();
+        }
+        return this;
+    }
+
+    AlignmentLimits deny(ExtendedFacing... deny){
+        if(deny!=null){
+            for (ExtendedFacing extendedFacing : deny) {
                 validStates[extendedFacing.getExtendedFacingIndex()]=false;
             }
         }
+        return this;
+    }
+
+    AlignmentLimits allow(ExtendedFacing... allow){
+        if(allow!=null){
+            for (ExtendedFacing extendedFacing : allow) {
+                validStates[extendedFacing.getExtendedFacingIndex()]=false;
+            }
+        }
+        return this;
+    }
+
+    AlignmentLimits deny(Direction... deny){
+        if(deny!=null){
+            for (ExtendedFacing value : ExtendedFacing.values()) {
+                for (Direction direction : deny) {
+                    if (value.getDirection() == direction) {
+                        validStates[value.getExtendedFacingIndex()] = false;
+                        break;
+                    }
+                }
+            }
+        }
+        return this;
+    }
+
+    AlignmentLimits allow(Direction... allow){
+        if(allow!=null){
+            for (ExtendedFacing value : ExtendedFacing.values()) {
+                for (Direction direction : allow) {
+                    if (value.getDirection() == direction) {
+                        validStates[value.getExtendedFacingIndex()] = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return this;
+    }
+
+    AlignmentLimits deny(Rotation... deny){
+        if(deny!=null){
+            for (ExtendedFacing value : ExtendedFacing.values()) {
+                for (Rotation rotation : deny) {
+                    if (value.getRotation() == rotation) {
+                        validStates[value.getExtendedFacingIndex()] = false;
+                        break;
+                    }
+                }
+            }
+        }
+        return this;
+    }
+
+    AlignmentLimits allow(Rotation... allow){
+        if(allow!=null){
+            for (ExtendedFacing value : ExtendedFacing.values()) {
+                for (Rotation rotation : allow) {
+                    if (value.getRotation() == rotation) {
+                        validStates[value.getExtendedFacingIndex()] = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return this;
+    }
+
+    AlignmentLimits deny(Flip... deny){
+        if(deny!=null){
+            for (ExtendedFacing value : ExtendedFacing.values()) {
+                for (Flip flip : deny) {
+                    if (value.getFlip() == flip) {
+                        validStates[value.getExtendedFacingIndex()] = false;
+                        break;
+                    }
+                }
+            }
+        }
+        return this;
+    }
+
+    AlignmentLimits allow(Flip... allow){
+        if(allow!=null){
+            for (ExtendedFacing value : ExtendedFacing.values()) {
+                for (Flip flip : allow) {
+                    if (value.getFlip() == flip) {
+                        validStates[value.getExtendedFacingIndex()] = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return this;
+    }
+
+    AlignmentLimits predicateApply(Predicate predicate){
+        for (ExtendedFacing value : ExtendedFacing.values()) {
+            predicate.apply(value).ifPresent(bool->validStates[value.getExtendedFacingIndex()]=bool);
+        }
+        return this;
+    }
+
+    public interface Predicate{
+        Optional<Boolean> apply(ExtendedFacing extendedFacing);
     }
 
     @Override
