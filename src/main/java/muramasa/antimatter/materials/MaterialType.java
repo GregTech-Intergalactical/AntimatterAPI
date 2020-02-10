8b586de3e034ca168ac7515c875776578c46398f
Rongmario <rongmario@gmail.com>
2020-02-10 15:43:43 +0000

    Moved MaterialItem stack methods to MaterialType

--- src/main/java/muramasa/antimatter/materials/MaterialType.java
+++ src/main/java/muramasa/antimatter/materials/MaterialType.java
@@ -4,6 +4,7 @@ import com.google.common.base.CaseFormat;
 import muramasa.antimatter.AntimatterAPI;
 import muramasa.antimatter.Ref;
 import muramasa.antimatter.Configs;
+import muramasa.antimatter.items.MaterialItem;
 import muramasa.antimatter.registration.IAntimatterObject;
 import muramasa.antimatter.util.Utils;
 import net.minecraft.item.Item;
@@ -106,9 +107,20 @@ public class MaterialType implements IMaterialTag, IAntimatterObject {
         return tagMap.get(this);
     }
 
-    //TODO
+    public Item get(Material material) {
+        return get(material, 1).getItem();
+    }
+
     public ItemStack get(Material material, int count) {
-        return ItemStack.EMPTY;
+        ItemStack replacement = AntimatterAPI.getReplacement(this, material);
+        if (!replacement.isEmpty()) return Utils.ca(count, replacement);
+        if (count == 0) Utils.onInvalidData("GET ERROR - COUNT 0: T(" + id + ") M(" + material.getId() + ")");
+        if (!allowGeneration(material)) Utils.onInvalidData("GET ERROR - DOES NOT GENERATE: T(" + id + ") M(" + material.getId() + ")");
+        MaterialItem item = AntimatterAPI.get(MaterialItem.class, id + "_" + material.getId());
+        if (item == null) Utils.onInvalidData("GET ERROR - MAT ITEM NULL: T(" + id + ") M(" + material.getId() + ")");
+        ItemStack stack = new ItemStack(item, count);
+        if (stack.isEmpty()) Utils.onInvalidData("GET ERROR - MAT STACK EMPTY: T(" + id + ") M(" + material.getId() + ")");
+        return stack;
     }
 
     @Override
@@ -130,11 +142,6 @@ public class MaterialType implements IMaterialTag, IAntimatterObject {
             if (!namePost.getFormattedText().isEmpty()) namePost = new StringTextComponent(" ").appendSibling(namePost);
         //}
         return new TranslationTextComponent("").appendSibling(namePre).appendSibling(material.getDisplayName()).appendSibling(namePost);
-    }
-
-    @Deprecated //TODO remove
-    public String oreName(Material material) {
-        return CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, getId().concat("_").concat(material.getId()));
     }
 
     public boolean allowGeneration(Material material) {
