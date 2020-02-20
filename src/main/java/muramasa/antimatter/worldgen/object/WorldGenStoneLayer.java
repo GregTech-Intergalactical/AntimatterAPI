220c44438656ec125843f0581b57e7c3ea144b94
Muramasa <haydenkilloh@gmail.com>
2020-02-20 00:08:37 +0000

    add special ores on vein collisions

--- src/main/java/muramasa/antimatter/worldgen/object/WorldGenStoneLayer.java
+++ src/main/java/muramasa/antimatter/worldgen/object/WorldGenStoneLayer.java
@@ -1,5 +1,6 @@
 package muramasa.antimatter.worldgen.object;
 
+import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 import muramasa.antimatter.blocks.BlockStone;
 import muramasa.antimatter.ore.StoneType;
 import muramasa.antimatter.worldgen.StoneLayerOre;
@@ -7,26 +8,42 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 
 import javax.annotation.Nullable;
-import java.util.Arrays;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class WorldGenStoneLayer extends WorldGenBase<WorldGenStoneLayer> {
+
+    private static Int2ObjectOpenHashMap<List<StoneLayerOre>> COLLISION_MAP = new Int2ObjectOpenHashMap<>();
 
     private StoneType stoneType;
     private BlockState stoneState;
     private StoneLayerOre[] ores = new StoneLayerOre[0];
 
-    public WorldGenStoneLayer(BlockState state, int... dims) {
+    protected WorldGenStoneLayer(BlockState state, int... dims) {
         super("world_gen_stone_layer", WorldGenStoneLayer.class, dims);
         this.stoneState = state;
     }
 
-    public WorldGenStoneLayer(Block block, int... dims) {
+    protected WorldGenStoneLayer(Block block, int... dims) {
         this(block.getDefaultState(), dims);
     }
 
-    public WorldGenStoneLayer(StoneType stoneType, int... dims) {
+    protected WorldGenStoneLayer(StoneType stoneType, int... dims) {
         this(stoneType.getState(), dims);
         this.stoneType = stoneType;
+    }
+
+    public static List<WorldGenStoneLayer> add(Block block, int weight, int... dims) {
+        return IntStream.of(weight).mapToObj(i -> new WorldGenStoneLayer(block.getDefaultState(), dims)).collect(Collectors.toList());
+    }
+
+    public static List<WorldGenStoneLayer> add(BlockState state, int weight, int... dims) {
+        return IntStream.of(weight).mapToObj(i -> new WorldGenStoneLayer(state, dims)).collect(Collectors.toList());
+    }
+
+    public static List<WorldGenStoneLayer> add(StoneType stoneType, int weight, int... dims) {
+        return IntStream.of(weight).mapToObj(i -> new WorldGenStoneLayer(stoneType, dims)).collect(Collectors.toList());
     }
 
     public WorldGenStoneLayer addOres(StoneLayerOre... ores) {
@@ -48,5 +65,19 @@ public class WorldGenStoneLayer extends WorldGenBase<WorldGenStoneLayer> {
 
     public StoneLayerOre[] getOres() {
         return ores;
+    }
+
+    public static void addCollision(BlockState top, BlockState bottom, StoneLayerOre... oresToAdd) {
+        COLLISION_MAP.computeIfAbsent(Objects.hash(top, bottom), k -> new ArrayList<>()).addAll(Arrays.asList(oresToAdd));
+    }
+
+    public static List<StoneLayerOre> getCollision(StoneType middle, BlockState top, BlockState bottom) {
+        if (middle == null) return Collections.emptyList();
+        List<StoneLayerOre> list = COLLISION_MAP.get(Objects.hash(top, bottom));
+        if (list == null) return Collections.emptyList();
+        for (StoneLayerOre ore : list) {
+            ore.setStatesByStoneType(middle);
+        }
+        return list;
     }
 }
