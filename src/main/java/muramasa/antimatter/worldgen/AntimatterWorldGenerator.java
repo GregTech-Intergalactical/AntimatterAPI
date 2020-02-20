677ef1e8c01a61f8da99b13184316ce97740a305
Muramasa <haydenkilloh@gmail.com>
2020-02-20 00:18:35 +0000

    move feature removal to AntimatterWorldGenerator

--- src/main/java/muramasa/antimatter/worldgen/AntimatterWorldGenerator.java
+++ src/main/java/muramasa/antimatter/worldgen/AntimatterWorldGenerator.java
@@ -2,10 +2,16 @@ package muramasa.antimatter.worldgen;
 
 import com.google.gson.JsonObject;
 import muramasa.antimatter.AntimatterAPI;
+import muramasa.antimatter.Configs;
 import muramasa.antimatter.registration.RegistrationEvent;
 import muramasa.antimatter.worldgen.feature.AntimatterFeature;
 import muramasa.antimatter.worldgen.object.WorldGenBase;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.gen.GenerationStage;
+import net.minecraft.world.gen.feature.*;
+import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -18,6 +24,8 @@ public class AntimatterWorldGenerator {
         try {
             //Path config = FMLPaths.CONFIGDIR.get().resolve("GregTech/WorldGenDefault.json");
             AntimatterAPI.onRegistration(RegistrationEvent.WORLDGEN_INIT);
+            if (Configs.WORLD.DISABLE_VANILLA_STONE_GEN) removeStoneFeatures();
+            if (Configs.WORLD.DISABLE_VANILLA_ORE_GEN) removeOreFeatures();
             AntimatterAPI.all(AntimatterFeature.class).stream().filter(AntimatterFeature::enabled).forEach(feat -> {
                 feat.onDataOverride(new JsonObject());
                 feat.init();
@@ -37,5 +45,65 @@ public class AntimatterWorldGenerator {
     public static <T> List<T> all(Class<T> c, int dim) {
         AntimatterFeature<?> feat = AntimatterAPI.get(AntimatterFeature.class, c.getName());
         return feat != null ? feat.getRegistry().computeIfAbsent(dim, k -> new LinkedList<>()).stream().map(c::cast).collect(Collectors.toList()) : Collections.emptyList();
+    }
+
+    private static void removeStoneFeatures() {
+        removeDecoratedFeatureFromAllBiomes(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE, Blocks.ANDESITE.getDefaultState(), Blocks.GRANITE.getDefaultState(), Blocks.DIORITE.getDefaultState());
+    }
+
+    private static void removeOreFeatures() {
+        removeDecoratedFeatureFromAllBiomes(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE, Blocks.COAL_ORE.getDefaultState(), Blocks.IRON_ORE.getDefaultState(), Blocks.GOLD_ORE.getDefaultState(), Blocks.REDSTONE_ORE.getDefaultState(), Blocks.LAPIS_ORE.getDefaultState(), Blocks.DIAMOND_ORE.getDefaultState());
+    }
+
+    /**
+     * Removes specific features, in specific generation stages, in specific biomes
+     * @param biomes set containing biomes wish to remove features from
+     * @param stage generation stage where the feature is added to
+     * @param featureToRemove feature instance wishing to be removed
+     * @param states BlockStates wish to be removed
+     */
+    public static void removeDecoratedFeaturesFromBiomes(Set<Biome> biomes, GenerationStage.Decoration stage, Feature<?> featureToRemove, BlockState... states) {
+        for (Biome biome : ForgeRegistries.BIOMES.getValues()) {
+            if (!biomes.contains(biome)) continue;
+            for (BlockState state : states) {
+                biome.getFeatures(stage).removeIf(f -> isDecoratedFeatureDisabled(f, featureToRemove, state));
+            }
+        }
+    }
+
+    /**
+     * Removes specific features, in specific generation stages, in all biomes registered
+     * @param stage generation stage where the feature is added to
+     * @param featureToRemove feature instance wishing to be removed
+     * @param states BlockStates wish to be removed
+     */
+    public static void removeDecoratedFeatureFromAllBiomes(GenerationStage.Decoration stage, Feature<?> featureToRemove, BlockState... states) {
+        for (Biome biome : ForgeRegistries.BIOMES.getValues()) {
+            for (BlockState state : states) {
+                biome.getFeatures(stage).removeIf(f -> isDecoratedFeatureDisabled(f, featureToRemove, state));
+            }
+        }
+    }
+
+    /**
+     * Check with BlockState in a feature if it is disabled
+     */
+    public static boolean isDecoratedFeatureDisabled(ConfiguredFeature<?, ?> configuredFeature, Feature<?> featureToRemove, BlockState state) {
+        if (configuredFeature.config instanceof DecoratedFeatureConfig) {
+            DecoratedFeatureConfig config = (DecoratedFeatureConfig) configuredFeature.config;
+            Feature<?> feature = config.feature.feature;
+            if (feature == featureToRemove) {
+                IFeatureConfig featureConfig = config.feature.config;
+                if (featureConfig instanceof OreFeatureConfig) {
+                    BlockState configState = ((OreFeatureConfig) featureConfig).state;
+                    if (configState != null && state == configState) return true;
+                }
+                if (featureConfig instanceof BlockStateFeatureConfig) {
+                    BlockState configState = ((BlockStateFeatureConfig) featureConfig).field_227270_a_;
+                    if (configState != null && state == configState) return true;
+                }
+            }
+        }
+        return false;
     }
 }
