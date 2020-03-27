e965e28984b13fe3649f61b2b3e59da506d29c1d
qubka <ushakovn@tcd.ie>
2020-03-27 19:42:01 +0000

    q

--- src/main/java/muramasa/antimatter/block/BlockBasic.java
+++ src/main/java/muramasa/antimatter/block/BlockBasic.java
@@ -1,18 +1,33 @@
 package muramasa.antimatter.block;
 
+import muramasa.antimatter.machine.Tier;
 import muramasa.antimatter.registration.IAntimatterObject;
 import muramasa.antimatter.registration.IModelProvider;
 import muramasa.antimatter.registration.ITextureProvider;
 import muramasa.antimatter.texture.Texture;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.Explosion;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.World;
+import tesseract.electric.ElectricHandler;
+import tesseract.electric.api.IElectricCable;
+import tesseract.util.Dir;
 
-public class BlockBasic extends Block implements IAntimatterObject, ITextureProvider, IModelProvider {
+import javax.annotation.Nullable;
+
+public class BlockBasic extends Block implements IAntimatterObject, ITextureProvider, IModelProvider, IElectricCable {
 
     public BlockBasic(Block.Properties properties) {
         super(properties);
     }
+
+    private ElectricHandler electricHandler;
 
     public BlockBasic() {
         this(Block.Properties.create(Material.IRON).hardnessAndResistance(1.0f, 1.0f).sound(SoundType.STONE));
@@ -22,4 +37,30 @@ public class BlockBasic extends Block implements IAntimatterObject, ITextureProv
     public Texture[] getTextures() {
         return new Texture[0];
     }
+
+    @Override
+    public long getVoltage() {
+        return 32;
+    }
+
+    @Override
+    public int getLoss() {
+        return 1;
+    }
+
+    @Override
+    public int getAmps() {
+        return 4;
+    }
+
+    @Override
+    public boolean connects(Dir direction) {
+        return true;
+    }
+
+    @Override
+    public void onBlockPlacedBy(World world, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack stack) {
+        electricHandler = new ElectricHandler(world.getDimension().getType().getId(), pos.toLong(), this);
+    }
+
 }
