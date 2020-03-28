7a84f3884ce2e49004197e1de84dcc8d7fe03f4b
qubka <ushakovn@tcd.ie>
2020-03-28 03:26:00 +0000

    Fixes

--- src/main/java/muramasa/antimatter/capability/impl/MachineEnergyHandler.java
+++ src/main/java/muramasa/antimatter/capability/impl/MachineEnergyHandler.java
@@ -1,22 +1,22 @@
 package muramasa.antimatter.capability.impl;
 
 import muramasa.antimatter.tile.TileEntityMachine;
-import tesseract.electric.ElectricHandler;
+import tesseract.electric.Electric;
 
 public class MachineEnergyHandler extends EnergyHandler {
 
-    protected ElectricHandler electricHandler;
+    protected Electric electric;
 
     public MachineEnergyHandler(TileEntityMachine tile) {
         super(0, 1, tile.getTier().getVoltage() * 64, tile.getTier().getVoltage(), tile.getTier().getVoltage());
-        electricHandler = new ElectricHandler(tile.getWorld().getDimension().getType().getId(), tile.getPos().toLong(), this);
+        electric = Electric.ofProducer(tile.getWorld().getDimension().getType().getId(), tile.getPos().toLong(), this);
     }
 
     public void update() {
-        electricHandler.send();
+        electric.update();
     }
 
     public void remove() {
-        electricHandler.remove();
+        electric.remove();
     }
 }
