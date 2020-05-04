21a1e939ba3653ecc9a177884f338903640ee78b
qubka <ushakovn@tcd.ie>
2020-05-04 19:51:04 +0100

    Add Container and Screen for pipe

---
+++ src/main/java/muramasa/antimatter/gui/container/ContainerPipe.java
@@ -0,0 +1,53 @@
+package muramasa.antimatter.gui.container;
+
+import muramasa.antimatter.gui.MenuHandler;
+import muramasa.antimatter.tile.TileEntityMachine;
+import muramasa.antimatter.tile.pipe.TileEntityPipe;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.util.IWorldPosCallable;
+
+public class ContainerPipe extends AntimatterContainer {
+
+    protected TileEntityPipe tile;
+    private int lastState = -1;
+
+    public ContainerPipe(TileEntityPipe tile, PlayerInventory playerInv, MenuHandler menuHandler, int windowId) {
+        super(menuHandler.getContainerType(), windowId, playerInv, 0);
+        //super(menuHandler.getContainerType(), windowId, playerInv, tile.getMachineType().getGui().getSlots(tile.getMachineTier()).size());
+        //addSlots(tile);
+        //if (tile.getMachineType().getGui().enablePlayerSlots()) addPlayerSlots();
+        this.tile = tile;
+    }
+
+    public TileEntityPipe getTile() {
+        return tile;
+    }
+
+    protected void addSlots(TileEntityMachine tile) {
+        /*tile.itemHandler.ifPresent(h -> {
+            int inputIndex = 0, outputIndex = 0, cellIndex = 0;
+            for (SlotData slot : tile.getMachineType().getGui().getSlots(tile.getMachineTier())) {
+                switch (slot.type) {
+                    case IT_IN:
+                        addSlot(new SlotInput(h.getInputWrapper(), inputIndex++, slot.x, slot.y));
+                        break;
+                    case IT_OUT:
+                        addSlot(new SlotOutput(h.getOutputWrapper(), outputIndex++, slot.x, slot.y));
+                        break;
+                    case CELL_IN:
+                        addSlot(new SlotInput(h.getCellWrapper(), cellIndex++, slot.x, slot.y));
+                        break;
+                    case CELL_OUT:
+                        addSlot(new SlotOutput(h.getCellWrapper(), cellIndex++, slot.x, slot.y));
+                        break;
+                }
+            }
+        });*/
+    }
+
+    @Override
+    public boolean canInteractWith(PlayerEntity player) {
+        return isWithinUsableDistance(IWorldPosCallable.of(tile.getWorld(), tile.getPos()), player, tile.getBlockState().getBlock());
+    }
+}
---
+++ src/main/java/muramasa/antimatter/gui/slot/SlotFake.java
@@ -0,0 +1,29 @@
+package muramasa.antimatter.gui.slot;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.SlotItemHandler;
+
+import javax.annotation.Nonnull;
+
+//TODO: Should be used on filters
+public class SlotFake extends SlotItemHandler {
+    public SlotFake(IItemHandler stackHandler, int index, int x, int y) {
+        super(stackHandler, index, x, y);
+    }
+
+    @Override
+    public boolean isItemValid(@Nonnull ItemStack stack) {
+        return false;
+    }
+
+    @Override
+    public boolean canTakeStack(PlayerEntity playerIn) {
+        return false;
+    }
+
+    @Override
+    public void onSlotChange(@Nonnull ItemStack p_75220_1_, @Nonnull ItemStack p_75220_2_) {
+    }
+}
