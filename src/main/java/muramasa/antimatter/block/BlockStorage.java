5cee7753f65270ee1cb629f5be747ba0183286e6
Rongmario <rongmario@gmail.com>
2020-05-20 03:52:34 +0100

    Restore Frames Functionality (climbable + stackable)

--- src/main/java/muramasa/antimatter/block/BlockStorage.java
+++ src/main/java/muramasa/antimatter/block/BlockStorage.java
@@ -8,88 +8,86 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.PushReaction;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.util.Direction;
+import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
+import net.minecraft.world.IBlockReader;
 import net.minecraft.world.IWorldReader;
-import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.world.World;
 import net.minecraftforge.common.ToolType;
 
 import javax.annotation.Nullable;
-import java.util.List;
 
 public class BlockStorage extends BlockMaterialType implements IItemBlockProvider, IColorHandler {
 
-    private static final AxisAlignedBB FRAME_COLLISION = new AxisAlignedBB(0.05, 0.0, 0.05, 0.95, 1.0, 0.95);//new AxisAlignedBB(0.0D, 0.0D, 0.0D, 0.0D, 0.0D, 0.0D);
+    private static final VoxelShape FRAME_SHAPE = VoxelShapes.create(0.05, 0.0, 0.05, 0.95, 1.0, 0.95);
     
     public BlockStorage(String domain, Material material, MaterialType<?> type) {
         super(domain, material, type, Block.Properties.create(net.minecraft.block.material.Material.IRON).hardnessAndResistance(8.0f).sound(SoundType.METAL));
     }
 
-    /** Frame Placing Stuffs - Start **/
-//    @Override
-//    public boolean onBlockActivated(World world, BlockPos pos, BlockState state, EntityPlayer player, EnumHand hand, Direction facing, float hitX, float hitY, float hitZ) {
-//        if (facing == Direction.UP) return false;
-//        ItemStack stack = player.getHeldItem(hand);
-//        if (stack.isEmpty()) return false;
-//        Item item = stack.getItem();
-//        if (!(item instanceof GTItemBlock)) return false;
-//        GTItemBlock itemBlock = ((GTItemBlock) item);
-//        Block block = itemBlock.getBlock();
-//        if (isFrame(block)) {
-//            BlockStorage frame = ((BlockStorage) block);
-//            BlockPos playerPos = player.getPosition();
-//            if (playerPos.equals(pos)) return false;
-//            MutableBlockPos mutablePos = new MutableBlockPos(pos);
-//            for (int i = pos.getY(); i < 256; i++) {
-//                mutablePos.move(Direction.UP);
-//                if (playerPos.equals(mutablePos) || player.isOnLadder()) return false;
-//                else if (world.mayPlace(frame, mutablePos, false, Direction.DOWN, player) && frame.canPlaceBlockAt(world, mutablePos)) {
-//                    world.setBlockState(mutablePos, frame.getDefaultState().withProperty(frame.getMaterialProp(), stack.getMetadata()));
-//                    if (!player.isCreative()) stack.shrink(1);
-//                    SoundType soundType = getSoundType();
-//                    world.playSound(player, mutablePos, soundType.getPlaceSound(), SoundCategory.BLOCKS, (soundType.getVolume() + 1.0F) / 2.0F, soundType.getPitch() * 0.8F);
-//                    return true;
-//                }
-//                else if (isFrame(world.getBlockState(mutablePos).getBlock())) continue;
-//                //else continue; //Uncomment this if we want frames to be place-able even if there are obstacles in the y-axis
-//                else break;
-//            }
-//        }
-//        return false;
-//    }
-    
-    private static boolean isFrame(Block block) {
-        return block instanceof BlockStorage && ((BlockStorage) block).type == MaterialType.FRAME;
-    }
-    /** Frame Placing Stuffs - End **/
+    @Override
+    public AntimatterItemBlock getItemBlock() {
 
-    /** Ladder Stuffs - Start **/
-//    @Override
-//    public void onEntityCollidedWithBlock(World world, BlockPos pos, BlockState state, Entity entityIn) {
-//        if (!(entityIn instanceof EntityLivingBase)) return;
-//        if (type == MaterialType.BLOCK) return;
-//        EntityLivingBase entity = (EntityLivingBase) entityIn;
-//        entity.motionX = MathHelper.clamp(entity.motionX, -0.15, 0.15);
-//        entity.motionZ = MathHelper.clamp(entity.motionZ, -0.15, 0.15);
-//        entity.fallDistance = 0.0F;
-//        if (entity.isSneaking() && entity instanceof EntityPlayer) {
-//            if (entity.isInWater()) entity.motionY = 0.02D;
-//            else entity.motionY = 0.08D;
-//        } else if (entity.collidedHorizontally)entity.motionY = 0.22D + (double)(getMaterialFromState(state).getToolSpeed() / 75);
-//        else entity.motionY = Math.max(entity.motionY, -0.2D);
-//    }
-//    
-//    @Override
-//    public AxisAlignedBB getCollisionBoundingBox(BlockState state, IBlockAccess world, BlockPos pos) {
-//        return type == MaterialType.FRAME ? FRAME_COLLISION : super.getCollisionBoundingBox(state, world, pos);
-//    }
+        return new AntimatterItemBlock(this) {
+
+            @Override
+            @Nullable
+            public BlockItemUseContext getBlockItemUseContext(BlockItemUseContext ctx) {
+                if (ctx.getPlayer().isCrouching()) return ctx;
+                BlockPos actualPos = ctx.getPos().offset(ctx.getFace().getOpposite());
+                Block currentBlock = ctx.getWorld().getBlockState(actualPos).getBlock();
+                // if (!(currentBlock instanceof BlockStorage) || ((BlockStorage) currentBlock).getType() != MaterialType.FRAME) return ctx; // Change to Block#isIn
+                if (!currentBlock.isIn(MaterialType.FRAME.getTag())) return ctx;
+                BlockPos.Mutable mutablePos = new BlockPos.Mutable(actualPos);
+                while (ctx.getWorld().getMaxHeight() > mutablePos.getY()) {
+                    if (ctx.getWorld().getBlockState(mutablePos.move(Direction.UP)).isReplaceable(ctx)) {
+                        ctx.getPlayer().swingArm(ctx.getHand());
+                        ctx.getWorld().setBlockState(mutablePos, ((BlockItem) ctx.getItem().getItem()).getBlock().getDefaultState());
+                        if (!ctx.getPlayer().isCreative()) ctx.getItem().shrink(1);
+                        ctx.getWorld().playSound(ctx.getPlayer(), mutablePos, soundType.getPlaceSound(), SoundCategory.BLOCKS, (soundType.getVolume() + 1.0F) / 2.0F, soundType.getPitch() * 0.8F);
+                        return null;
+                    }
+                }
+                return ctx;
+            }
+
+        };
+
+    }
 
     @Override
-    public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
-        return super.getDrops(state, builder);
+    public void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity) {
+        if (!(entity instanceof LivingEntity)) return;
+        if (type == MaterialType.BLOCK) return;
+        entity.setMotion(MathHelper.clamp(entity.getMotion().x, -0.15, 0.15), entity.getMotion().y, MathHelper.clamp(entity.getMotion().z, -0.15, 0.15));
+        entity.fallDistance = 0.0F;
+        if (entity.isCrouching() && entity instanceof PlayerEntity) {
+            if (entity.isInWater()) entity.setMotion(entity.getMotion().x, 0.02D, entity.getMotion().z);
+            else entity.setMotion(entity.getMotion().x, 0.08D, entity.getMotion().z);
+        } else if (entity.collidedHorizontally) entity.setMotion(entity.getMotion().x, 0.22D + (material.getToolSpeed() / 75), entity.getMotion().z);
+        else entity.setMotion(entity.getMotion().x, Math.max(entity.getMotion().y, -0.2D), entity.getMotion().z);
     }
+
+    @Override
+    public VoxelShape getShape(BlockState state, IBlockReader world, BlockPos pos, ISelectionContext context) {
+        if (context != ISelectionContext.dummy()) return super.getShape(state, world, pos, context);
+        return type == MaterialType.FRAME ? FRAME_SHAPE : super.getShape(state, world, pos, context);
+    }
+
+    // @Override
+    // public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+        // return super.getDrops(state, builder);
+    // }
 
     /** Ladder Stuffs - End **/
     
