32cff2d8c7e35b862ee74b663a50056fa004f8af
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2020-06-20 23:17:55 -0400

    finished active support and added layered support for six sided blocks, though in a limited fashion

--- src/main/java/muramasa/antimatter/datagen/providers/AntimatterBlockStateProvider.java
+++ src/main/java/muramasa/antimatter/datagen/providers/AntimatterBlockStateProvider.java
@@ -13,6 +13,7 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.util.Direction;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.model.generators.BlockModelBuilder;
@@ -85,6 +86,10 @@ public class AntimatterBlockStateProvider extends BlockStateProvider implements 
         return (AntimatterBlockModelBuilder) models().getBuilder(block.getRegistryName().getPath());
     }
 
+    public AntimatterBlockModelBuilder getActiveBuilder(Block block) {
+        return (AntimatterBlockModelBuilder) models().getBuilder(block.getRegistryName().getPath() + "_active");
+    }
+
     public BlockModelBuilder cubeAll(Block block, ResourceLocation texture) {
         return models().cubeAll(block.getRegistryName().toString(), texture);
     }
@@ -98,11 +103,12 @@ public class AntimatterBlockStateProvider extends BlockStateProvider implements 
             simpleBlock(block, getSimpleModel(block, textures[0]));
         } else if (textures.length == 2) {
             simpleBlock(block, getLayeredModel(block, textures[0], textures[1]));
-        }
-        else if (textures.length == 6){
-            horizontalBlock(block, getSixSidedSimpleModel(block, textures));
+        } else if (textures.length == 6){
+            horizontalBlock(block, getSixSidedSimpleModel(block, false, textures));
         } else if (textures.length == 12){
-            horizontalBlock(block, getSixSidedSimpleModel(block, textures), getSixSidedSimpleModel(block, Arrays.copyOfRange(textures, 6, 12)));
+            horizontalBlock(block, getSixSidedSimpleModel(block, false, textures), getSixSidedSimpleModel(block, true, Arrays.copyOfRange(textures, 6, 12)));
+        } else if (textures.length == 24){
+            horizontalBlock(block, getSixSidedLayeredModel(block, false, textures), getSixSidedLayeredModel(block, true, Arrays.copyOfRange(textures, 12, 24)));
         }
     }
 
@@ -113,8 +119,18 @@ public class AntimatterBlockStateProvider extends BlockStateProvider implements 
     public void horizontalBlock(Block block, Function<BlockState, ModelFile> modelFunc, Function<BlockState, ModelFile> modelFuncActive) {
         getVariantBuilder(block)
                 .forAllStates(state -> ConfiguredModel.builder()
-                        .modelFile(state.get(BlockBasic.ACTIVE) ? modelFuncActive.apply(state) : modelFunc.apply(state))
-                        .rotationY(((int) state.get(BlockStateProperties.HORIZONTAL_FACING).getHorizontalAngle()) % 360)
+                        .modelFile(state.has(BlockBasic.ACTIVE) && state.get(BlockBasic.ACTIVE) ? modelFuncActive.apply(state) : modelFunc.apply(state))
+                        .rotationY((state.has(BlockStateProperties.HORIZONTAL_FACING) ? (int) state.get(BlockStateProperties.HORIZONTAL_FACING).getHorizontalAngle() : (int) Direction.SOUTH.getHorizontalAngle()) % 360)
+                        .build()
+                );
+    }
+
+    @Override
+    public void horizontalBlock(Block block, Function<BlockState, ModelFile> modelFunc) {
+        getVariantBuilder(block)
+                .forAllStates(state -> ConfiguredModel.builder()
+                        .modelFile(modelFunc.apply(state))
+                        .rotationY((state.has(BlockStateProperties.HORIZONTAL_FACING) ? (int) state.get(BlockStateProperties.HORIZONTAL_FACING).getHorizontalAngle() : (int) Direction.SOUTH.getHorizontalAngle()) % 360)
                         .build()
                 );
     }
@@ -123,8 +139,14 @@ public class AntimatterBlockStateProvider extends BlockStateProvider implements 
         return getBuilder(block).parent(models().getExistingFile(loc(Ref.ID, "block/preset/simple"))).texture("all", texture);
     }
 
-    public BlockModelBuilder getSixSidedSimpleModel(Block block, ResourceLocation... texture) {
-        return getBuilder(block).parent(models().getExistingFile(loc(Ref.ID, "block/preset/simple"))).texture("down", texture[0]).texture("up", texture[1]).texture("north", texture[2]).texture("south", texture[3]).texture("west", texture[4]).texture("east", texture[5]);
+    public BlockModelBuilder getSixSidedSimpleModel(Block block, boolean active, ResourceLocation... texture) {
+        BlockModelBuilder builder = active ? getActiveBuilder(block) : getBuilder(block);
+        return builder.parent(models().getExistingFile(loc(Ref.ID, "block/preset/simple"))).texture("down", texture[0]).texture("up", texture[1]).texture("south", texture[2]).texture("north", texture[3]).texture("west", texture[4]).texture("east", texture[5]).texture("particle", texture[1]);
+    }
+
+    public BlockModelBuilder getSixSidedLayeredModel(Block block, boolean active, ResourceLocation... texture) {
+        BlockModelBuilder builder = active ? getActiveBuilder(block) : getBuilder(block);
+        return builder.parent(models().getExistingFile(loc(Ref.ID, "block/preset/layered"))).texture("basedown", texture[0]).texture("baseup", texture[1]).texture("basesouth", texture[2]).texture("basenorth", texture[3]).texture("basewest", texture[4]).texture("baseeast", texture[5]).texture("overlaydown", texture[6]).texture("overlayup", texture[7]).texture("overlaysouth", texture[8]).texture("overlaynorth", texture[9]).texture("overlaywest", texture[10]).texture("overlayeast", texture[11]).texture("particle", texture[1]);
     }
 
     public BlockModelBuilder getLayeredModel(Block block, ResourceLocation base, ResourceLocation overlay) {
