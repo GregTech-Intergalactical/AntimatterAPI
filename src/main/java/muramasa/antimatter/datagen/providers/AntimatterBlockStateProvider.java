02b54a02ea31adbf742bac53c48207f803789865
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2020-06-22 18:19:08 -0400

    requested changes

--- src/main/java/muramasa/antimatter/datagen/providers/AntimatterBlockStateProvider.java
+++ src/main/java/muramasa/antimatter/datagen/providers/AntimatterBlockStateProvider.java
@@ -9,23 +9,18 @@ import muramasa.antimatter.datagen.builder.AntimatterBlockModelBuilder;
 import muramasa.antimatter.datagen.resources.DynamicResourcePack;
 import muramasa.antimatter.fluid.AntimatterFluid;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.state.properties.BlockStateProperties;
-import net.minecraft.util.Direction;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.model.generators.BlockModelBuilder;
 import net.minecraftforge.client.model.generators.BlockModelProvider;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
-import net.minecraftforge.client.model.generators.ConfiguredModel;
 import net.minecraftforge.client.model.generators.ExistingFileHelper;
 import net.minecraftforge.client.model.generators.IGeneratedBlockstate;
 import net.minecraftforge.client.model.generators.ModelFile;
 
 import javax.annotation.Nonnull;
 import java.util.Map;
-import java.util.function.Function;
 
 public class AntimatterBlockStateProvider extends BlockStateProvider implements IAntimatterProvider {
 
@@ -84,10 +79,6 @@ public class AntimatterBlockStateProvider extends BlockStateProvider implements 
         return (AntimatterBlockModelBuilder) models().getBuilder(block.getRegistryName().getPath());
     }
 
-    public AntimatterBlockModelBuilder getActiveBuilder(Block block) {
-        return (AntimatterBlockModelBuilder) models().getBuilder(block.getRegistryName().getPath() + "_active");
-    }
-
     public BlockModelBuilder cubeAll(Block block, ResourceLocation texture) {
         return models().cubeAll(block.getRegistryName().toString(), texture);
     }
@@ -102,34 +93,22 @@ public class AntimatterBlockStateProvider extends BlockStateProvider implements 
         } else if (textures.length == 2) {
             simpleBlock(block, getLayeredModel(block, textures[0], textures[1]));
         } else if (textures.length == 6){
-            horizontalBlock(block, getSixSidedSimpleModel(block, false, textures));
+            horizontalBlock(block, getSixSidedSimpleModel(block, textures));
         } else if (textures.length == 12){
-            horizontalBlock(block, getSixSidedLayeredModel(block, false, textures));
+            horizontalBlock(block, getSixSidedLayeredModel(block, textures));
         }
-    }
-
-    @Override
-    public void horizontalBlock(Block block, Function<BlockState, ModelFile> modelFunc) {
-        getVariantBuilder(block)
-                .forAllStates(state -> ConfiguredModel.builder()
-                        .modelFile(modelFunc.apply(state))
-                        .rotationY((state.has(BlockStateProperties.HORIZONTAL_FACING) ? (int) state.get(BlockStateProperties.HORIZONTAL_FACING).getHorizontalAngle() : (int) Direction.SOUTH.getHorizontalAngle()) % 360)
-                        .build()
-                );
     }
 
     public BlockModelBuilder getSimpleModel(Block block, ResourceLocation texture) {
         return getBuilder(block).parent(models().getExistingFile(loc(Ref.ID, "block/preset/simple"))).texture("all", texture);
     }
 
-    public BlockModelBuilder getSixSidedSimpleModel(Block block, boolean active, ResourceLocation... texture) {
-        BlockModelBuilder builder = active ? getActiveBuilder(block) : getBuilder(block);
-        return builder.parent(models().getExistingFile(loc(Ref.ID, "block/preset/simple"))).texture("down", texture[0]).texture("up", texture[1]).texture("south", texture[2]).texture("north", texture[3]).texture("west", texture[4]).texture("east", texture[5]).texture("particle", texture[1]);
+    public BlockModelBuilder getSixSidedSimpleModel(Block block, ResourceLocation... texture) {
+        return getSimpleModel(block, texture[0]).texture("down", texture[0]).texture("up", texture[1]).texture("south", texture[2]).texture("north", texture[3]).texture("west", texture[4]).texture("east", texture[5]).texture("particle", texture[1]);
     }
 
-    public BlockModelBuilder getSixSidedLayeredModel(Block block, boolean active, ResourceLocation... texture) {
-        BlockModelBuilder builder = active ? getActiveBuilder(block) : getBuilder(block);
-        return builder.parent(models().getExistingFile(loc(Ref.ID, "block/preset/layered"))).texture("basedown", texture[0]).texture("baseup", texture[1]).texture("basesouth", texture[2]).texture("basenorth", texture[3]).texture("basewest", texture[4]).texture("baseeast", texture[5]).texture("overlaydown", texture[6]).texture("overlayup", texture[7]).texture("overlaysouth", texture[8]).texture("overlaynorth", texture[9]).texture("overlaywest", texture[10]).texture("overlayeast", texture[11]).texture("particle", texture[1]);
+    public BlockModelBuilder getSixSidedLayeredModel(Block block, ResourceLocation... texture) {
+        return getLayeredModel(block, texture[0], texture[6]).texture("basedown", texture[0]).texture("baseup", texture[1]).texture("basesouth", texture[2]).texture("basenorth", texture[3]).texture("basewest", texture[4]).texture("baseeast", texture[5]).texture("overlaydown", texture[6]).texture("overlayup", texture[7]).texture("overlaysouth", texture[8]).texture("overlaynorth", texture[9]).texture("overlaywest", texture[10]).texture("overlayeast", texture[11]).texture("particle", texture[1]);
     }
 
     public BlockModelBuilder getLayeredModel(Block block, ResourceLocation base, ResourceLocation overlay) {
