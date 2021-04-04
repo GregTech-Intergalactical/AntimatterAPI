0fc60fcefd2539424642d33ba913eae6cab73437
Abbe <albertsjolund@hotmail.com>
2021-04-04 13:24:53 +0200

    Fix compile issue of tools.
    Closes #123

--- src/main/java/muramasa/antimatter/tool/armor/MaterialArmor.java
+++ src/main/java/muramasa/antimatter/tool/armor/MaterialArmor.java
@@ -30,9 +30,7 @@ import javax.annotation.Nullable;
 import java.util.List;
 import java.util.UUID;
 
-import static muramasa.antimatter.Data.GEM;
-import static muramasa.antimatter.Data.HELMET;
-import static muramasa.antimatter.Data.INGOT;
+import static muramasa.antimatter.Data.*;
 
 public class MaterialArmor extends ArmorItem implements IAntimatterArmor, IDyeableArmorItem {
     private static final UUID[] ARMOR_MODIFIERS = new UUID[]{UUID.fromString("845DB27C-C624-495F-8C9F-6020A9A58B6B"), UUID.fromString("D8499B04-0E66-4726-AB29-64469D734E0D"), UUID.fromString("9F3D476D-C118-4544-8365-64846904B48E"), UUID.fromString("2AD3F246-FEE1-4E67-B886-69FD380BB150")};
@@ -75,7 +73,7 @@ public class MaterialArmor extends ArmorItem implements IAntimatterArmor, IDyeab
     @Override
     public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlotType slotType, ItemStack stack) {
         Material mat = getMaterial(stack);
-        if (mat == null || slotType != this.slot) return super.getAttributeModifiers(slotType, stack);
+        if (mat == null || mat == NULL || slotType != this.slot) return super.getAttributeModifiers(slotType, stack);
         Multimap<Attribute, AttributeModifier> modifiers = HashMultimap.create();
         UUID uuid = ARMOR_MODIFIERS[slot.getIndex()];
         modifiers.put(Attributes.ARMOR, new AttributeModifier(uuid, "Armor modifier", material.getDamageReductionAmount(slot) + mat.getArmor()[slot.getIndex()], AttributeModifier.Operation.ADDITION));
