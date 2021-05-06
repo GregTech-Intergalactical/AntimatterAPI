d360ce11454f12daf3eb4d543f6473568a191f68
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2021-05-06 18:56:24 -0400

    added gui stuff

--- src/main/java/muramasa/antimatter/cover/ICoverModeHandler.java
+++ src/main/java/muramasa/antimatter/cover/ICoverModeHandler.java
@@ -2,4 +2,12 @@ package muramasa.antimatter.cover;
 
 public interface ICoverModeHandler {
     ICoverMode getCoverMode(CoverStack<?> stack);
+
+    default int getOverlayX(){
+        return 176;
+    }
+
+    default int getOverlayY(){
+        return 0;
+    }
 }
