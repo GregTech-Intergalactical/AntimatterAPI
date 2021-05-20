ea90d5175b1249a1494be8364cc818a198ee5d75
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2021-05-20 19:58:35 -0400

    added methods to BlockLootTablesAccessor

--- src/main/java/muramasa/antimatter/mixin/BlockLootTablesAccessor.java
+++ src/main/java/muramasa/antimatter/mixin/BlockLootTablesAccessor.java
@@ -11,4 +11,24 @@ public interface BlockLootTablesAccessor {
     public static ILootCondition.IBuilder getSilkTouchOrShears() {
         throw new AssertionError();
     }
+
+    @Accessor("NO_SILK_TOUCH")
+    public static ILootCondition.IBuilder getNoSilkTouch() {
+        throw new AssertionError();
+    }
+
+    @Accessor("SILK_TOUCH")
+    public static ILootCondition.IBuilder getSilkTouch() {
+        throw new AssertionError();
+    }
+
+    @Accessor("SHEARS")
+    public static ILootCondition.IBuilder getShears() {
+        throw new AssertionError();
+    }
+
+    @Accessor("NOT_SILK_TOUCH_OR_SHEARS")
+    public static ILootCondition.IBuilder getNotSilkTouchOrShears() {
+        throw new AssertionError();
+    }
 }
