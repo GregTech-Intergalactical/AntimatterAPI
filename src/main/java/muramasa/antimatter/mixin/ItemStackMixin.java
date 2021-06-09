918d52f32ebf51b85575adbf82150b71e8c7e45b
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2021-06-09 00:26:58 -0400

    removed imports

--- src/main/java/muramasa/antimatter/mixin/ItemStackMixin.java
+++ src/main/java/muramasa/antimatter/mixin/ItemStackMixin.java
@@ -1,26 +1,14 @@
 package muramasa.antimatter.mixin;
 
-import muramasa.antimatter.Antimatter;
-import muramasa.antimatter.tool.AntimatterToolType;
 import muramasa.antimatter.tool.IAntimatterTool;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
-import net.minecraft.util.IItemProvider;
-import net.minecraft.world.World;
-import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import javax.annotation.Nullable;
-import java.util.Random;
 import java.util.function.Consumer;
 
 @Mixin(ItemStack.class)
