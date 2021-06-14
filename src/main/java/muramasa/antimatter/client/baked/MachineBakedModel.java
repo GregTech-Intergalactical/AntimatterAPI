b74f2c0e087ec416471d667664093e579dd00ed0
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2021-06-14 00:22:13 -0400

    added check for vertical facing

--- src/main/java/muramasa/antimatter/client/baked/MachineBakedModel.java
+++ src/main/java/muramasa/antimatter/client/baked/MachineBakedModel.java
@@ -43,7 +43,7 @@ public class MachineBakedModel extends CoveredBakedModel {
                 MachineCoverHandler<?> covers = t.coverHandler.orElse(null);
                 CoverStack<?> c = covers == null ? null : covers.get(side);
                 if (c == null || c.skipRender()) {
-                    TileEntityMachine.DynamicKey key = new TileEntityMachine.DynamicKey(new ResourceLocation(bm.getType().getId()), tex, state.get(BlockStateProperties.HORIZONTAL_FACING), data.getData(AntimatterProperties.MACHINE_STATE));
+                    TileEntityMachine.DynamicKey key = new TileEntityMachine.DynamicKey(new ResourceLocation(bm.getType().getId()), tex, state.get((bm.getType().allowVerticalFacing() ? BlockStateProperties.FACING : BlockStateProperties.HORIZONTAL_FACING)), data.getData(AntimatterProperties.MACHINE_STATE));
                     quads = t.multiTexturer.getValue().getQuads(quads, state, t, key, side.getIndex(), data);
                     assert quads.size() == 0 || quads.get(0).getFace() == side;
                 }
