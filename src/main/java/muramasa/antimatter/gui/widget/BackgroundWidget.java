2b17c6cdbabba425455e7fe1f4d97133ce2c0ad7
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2021-09-19 19:58:47 -0400

    added guiwidth and gui height options to background widget

--- src/main/java/muramasa/antimatter/gui/widget/BackgroundWidget.java
+++ src/main/java/muramasa/antimatter/gui/widget/BackgroundWidget.java
@@ -1,20 +1,27 @@
 package muramasa.antimatter.gui.widget;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.systems.RenderSystem;
 import muramasa.antimatter.gui.GuiInstance;
 import muramasa.antimatter.gui.IGuiElement;
 import muramasa.antimatter.gui.Widget;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.AbstractGui;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
 public class BackgroundWidget extends Widget {
 
-    private final int xSize, ySize;
+    private final int xSize, ySize, guiXSize, guiYSize;
     private final ResourceLocation loc;
 
-    protected BackgroundWidget(GuiInstance instance, IGuiElement parent, ResourceLocation loc, int xSize, int ySize) {
+    protected BackgroundWidget(GuiInstance instance, IGuiElement parent, ResourceLocation loc, int xSize, int ySize, int guiXSize, int guiYSize) {
         super(instance, parent);
         this.xSize = xSize;
         this.ySize = ySize;
+        this.guiXSize = guiXSize;
+        this.guiYSize = guiYSize;
         this.loc = loc;
         setDepth(-1);
     }
@@ -26,12 +33,25 @@ public class BackgroundWidget extends Widget {
     }
 
     public static WidgetSupplier build(ResourceLocation loc, int w, int h) {
-        return builder((a,b) -> new BackgroundWidget(a,b, loc, w, h)).clientSide();
+        return builder((a,b) -> new BackgroundWidget(a,b, loc, w, h, 256, 256)).clientSide();
+    }
+
+    public static WidgetSupplier build(ResourceLocation loc, int w, int h, int guiW, int guiH) {
+        return builder((a,b) -> new BackgroundWidget(a,b, loc, w, h, guiW, guiH)).clientSide();
     }
 
 
     @Override
     public void render(MatrixStack matrixStack, double mouseX, double mouseY, float partialTicks) {
         drawTexture(matrixStack, loc, realX(),realY(), 0,0, xSize, ySize);
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @Override
+    protected void drawTexture(MatrixStack stack, ResourceLocation loc, int left, int top, int x, int y, int sizeX, int sizeY) {
+        RenderSystem.color4f(1, 1, 1, 1);
+        Minecraft.getInstance().textureManager.bindTexture(loc);
+        //AbstractGui.blit(stack, left, top, x, y, sizeX, sizeY);
+        AbstractGui.blit(stack, left, top, x, y, sizeX, sizeY, this.guiXSize, this.guiYSize);
     }
 }
