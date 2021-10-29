c6f83f44a4cfd0440a5a232d381c135211d62054
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2021-10-29 15:41:09 -0400

    added remapping event

--- src/main/java/muramasa/antimatter/common/event/CommonEvents.java
+++ src/main/java/muramasa/antimatter/common/event/CommonEvents.java
@@ -1,13 +1,18 @@
 package muramasa.antimatter.common.event;
 
+import muramasa.antimatter.AntimatterAPI;
 import muramasa.antimatter.AntimatterConfig;
+import muramasa.antimatter.Data;
 import muramasa.antimatter.Ref;
 import muramasa.antimatter.datagen.providers.AntimatterBlockLootProvider;
+import muramasa.antimatter.material.Material;
+import muramasa.antimatter.ore.BlockOre;
 import muramasa.antimatter.tool.IAntimatterArmor;
 import muramasa.antimatter.tool.IAntimatterTool;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.inventory.IInventory;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.loot.ConstantRange;
 import net.minecraft.loot.ItemLootEntry;
@@ -16,11 +21,14 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
 import net.minecraftforge.event.AnvilUpdateEvent;
 import net.minecraftforge.event.LootTableLoadEvent;
+import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.ForgeRegistries;
+
+import static muramasa.antimatter.Data.NULL;
 
 @Mod.EventBusSubscriber(modid = Ref.ID)
 public class CommonEvents {
@@ -72,6 +80,42 @@ public class CommonEvents {
                 if (block == Blocks.ICE || block == Blocks.PACKED_ICE || block == Blocks.BLUE_ICE) {
                     event.getTable().addPool(LootPool.builder().rolls(ConstantRange.of(1)).acceptCondition(AntimatterBlockLootProvider.SAW).addEntry(ItemLootEntry.builder(block)).build());
                 }
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void remapMissingBlocks(final RegistryEvent.MissingMappings<Block> event){
+        for (RegistryEvent.MissingMappings.Mapping<Block> map : event.getMappings(Ref.MOD_KJS)) {
+            String domain = map.key.getNamespace();
+            String id = map.key.getPath();
+            if (id.startsWith("block_")){
+                Material mat = Material.get(id.replace("block_", ""));
+                if (mat != NULL){
+                    map.remap(Data.BLOCK.get().get(mat).asBlock());
+                    continue;
+                }
+            }
+            if (id.startsWith("ore_")){
+                Block replacement = AntimatterAPI.get(BlockOre.class, id);
+                if (replacement != null){
+                    map.remap(replacement);
+                    continue;
+                }
+            }
+            Block replacement = AntimatterAPI.get(Block.class, id, Ref.SHARED_ID);
+            if (replacement != null){
+                map.remap(replacement);
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void remapMissingItems(final RegistryEvent.MissingMappings<Item> event){
+        for (RegistryEvent.MissingMappings.Mapping<Item> map : event.getMappings(Ref.ID)) {
+            Item replacement = AntimatterAPI.get(Item.class, map.key.getPath(), Ref.SHARED_ID);
+            if (replacement != null){
+                map.remap(replacement);
             }
         }
     }
