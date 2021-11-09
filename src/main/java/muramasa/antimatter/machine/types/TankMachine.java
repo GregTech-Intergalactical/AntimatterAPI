b67c79f9c7cd1923b4a324c1385aa84c44d05aab
Abbe <albertsjolund@hotmail.com>
2021-11-09 09:55:47 +0100

    Make tank sync info

--- src/main/java/muramasa/antimatter/machine/types/TankMachine.java
+++ src/main/java/muramasa/antimatter/machine/types/TankMachine.java
@@ -1,7 +1,14 @@
 package muramasa.antimatter.machine.types;
 
 import muramasa.antimatter.Data;
+import muramasa.antimatter.gui.GuiInstance;
+import muramasa.antimatter.gui.ICanSyncData;
+import muramasa.antimatter.gui.IGuiElement;
+import muramasa.antimatter.gui.widget.InfoRenderWidget;
+import muramasa.antimatter.gui.widget.WidgetSupplier;
+import muramasa.antimatter.integration.jei.renderer.IInfoRenderer;
 import muramasa.antimatter.tile.TileEntityTank;
+import net.minecraftforge.fluids.FluidStack;
 
 import static muramasa.antimatter.machine.MachineFlag.*;
 
@@ -14,5 +21,31 @@ public class TankMachine extends Machine<TankMachine> {
         setGUI(Data.BASIC_MENU_HANDLER);
         frontCovers();
         allowFrontIO();
+    }
+
+    @Override
+    protected void setupGui() {
+        super.setupGui();
+        addGuiCallback(t -> t.addWidget(TankRenderWidget.build()));
+    }
+
+    public static class TankRenderWidget extends InfoRenderWidget<TankRenderWidget> {
+
+        public FluidStack stack;
+
+        protected TankRenderWidget(GuiInstance gui, IGuiElement parent, IInfoRenderer<TankRenderWidget> renderer) {
+            super(gui, parent, renderer);
+        }
+
+        @Override
+        public void init() {
+            super.init();
+            TileEntityTank<?> tank = (TileEntityTank<?>)gui.handler;
+            gui.syncFluidStack(() -> tank.fluidHandler.map(t -> t.getFluidInTank(0)).orElse(FluidStack.EMPTY), f -> this.stack = f, ICanSyncData.SyncDirection.SERVER_TO_CLIENT);
+        }
+
+        public static WidgetSupplier build() {
+            return builder((a, b) -> new TankRenderWidget(a, b, (IInfoRenderer<TankRenderWidget>) a.handler));
+        }
     }
 }
--- src/main/java/muramasa/antimatter/tile/TileEntityTank.java
+++ src/main/java/muramasa/antimatter/tile/TileEntityTank.java
@@ -1,13 +1,17 @@
 package muramasa.antimatter.tile;
 
+import com.mojang.blaze3d.matrix.MatrixStack;
 import muramasa.antimatter.capability.fluid.FluidTanks;
 import muramasa.antimatter.capability.machine.MachineFluidHandler;
+import muramasa.antimatter.integration.jei.renderer.IInfoRenderer;
 import muramasa.antimatter.machine.types.Machine;
+import muramasa.antimatter.machine.types.TankMachine;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraftforge.fluids.capability.templates.FluidTank;
 
 import javax.annotation.Nullable;
 
-public class TileEntityTank<T extends TileEntityMachine<T>> extends TileEntityMachine<T> {
+public class TileEntityTank<T extends TileEntityMachine<T>> extends TileEntityMachine<T> implements IInfoRenderer<TankMachine.TankRenderWidget> {
 
     public TileEntityTank(Machine<?> type) {
         super(type);
@@ -28,5 +32,14 @@ public class TileEntityTank<T extends TileEntityMachine<T>> extends TileEntityMa
                 return getInputTanks();
             }
         });
+    }
+
+    @Override
+    public int drawInfo(TankMachine.TankRenderWidget instance, MatrixStack stack, FontRenderer renderer, int left, int top) {
+        left = left + 55;
+        top = top + 24;
+        renderer.drawString(stack, instance.stack.getDisplayName().getString(), left, top, 16448255);
+        renderer.drawString(stack, instance.stack.getAmount() + " mb", left, top + 8, 16448255);
+        return 16;
     }
 }
