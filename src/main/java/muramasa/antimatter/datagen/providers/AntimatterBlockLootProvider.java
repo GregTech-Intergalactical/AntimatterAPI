c86914f9111be31d0f1eb1c2b1b17b0673ec1334
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2022-01-31 19:42:42 -0500

    changed data generator field to protected

--- src/main/java/muramasa/antimatter/datagen/providers/AntimatterBlockLootProvider.java
+++ src/main/java/muramasa/antimatter/datagen/providers/AntimatterBlockLootProvider.java
@@ -38,7 +38,7 @@ import static muramasa.antimatter.Data.*;
 
 public class AntimatterBlockLootProvider extends BlockLootTables implements IDataProvider, IAntimatterProvider {
     protected final String providerDomain, providerName;
-    private final DataGenerator generator;
+    protected final DataGenerator generator;
     protected final Map<Block, Function<Block, LootTable.Builder>> tables = new Object2ObjectOpenHashMap<>();
 
     public static final ILootCondition.IBuilder BRANCH_CUTTER = MatchTool.toolMatches(ItemPredicate.Builder.item().of(Data.BRANCH_CUTTER.getToolStack(Data.NULL, Data.NULL).getItem()));
