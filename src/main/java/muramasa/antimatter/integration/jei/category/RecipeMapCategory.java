83da8bb1c994642f25d6d39f4e70f85562fb1d56
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2022-02-27 21:53:14 -0500

    fixed any future issues with null machine icons in jei

--- src/main/java/muramasa/antimatter/integration/jei/category/RecipeMapCategory.java
+++ src/main/java/muramasa/antimatter/integration/jei/category/RecipeMapCategory.java
@@ -74,9 +74,7 @@ public class RecipeMapCategory implements IRecipeCategory<Recipe> {
                 this.icon = (IDrawable) icon;
             }
         } else {
-            Block block = AntimatterAPI.get(BlockMachine.class, blockItemModel == null ? "" : blockItemModel.getPath() + "_" + defaultTier.getId(), blockItemModel == null ? "" : blockItemModel.getNamespace());
-            if (block == null)
-                block = AntimatterAPI.get(BlockMultiMachine.class, blockItemModel == null ? "" : blockItemModel.getPath() + "_" + defaultTier.getId(), blockItemModel == null ? "" : blockItemModel.getNamespace());
+            Block block = AntimatterAPI.get(Block.class, blockItemModel == null ? "" : blockItemModel.getPath() + "_" + defaultTier.getId(), blockItemModel == null ? "" : blockItemModel.getNamespace());
             this.icon = block == null ? guiHelper.createDrawableIngredient(new ItemStack(Data.DEBUG_SCANNER, 1)) : guiHelper.createDrawableIngredient(new ItemStack(block.asItem(), 1));
         }
         this.gui = gui;
