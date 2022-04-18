aee8a1fa07cb066da879dfa97f4c02798dd87a32
Abbe <albertsjolund@hotmail.com>
2022-04-18 20:27:48 +0200

    add height check

--- src/main/java/muramasa/antimatter/integration/jei/extension/JEIMaterialRecipeExtension.java
+++ src/main/java/muramasa/antimatter/integration/jei/extension/JEIMaterialRecipeExtension.java
@@ -142,7 +142,7 @@ public record JEIMaterialRecipeExtension(MaterialRecipe recipe) implements ICraf
             for (int y = 0; y < 3; ++y) {
                 for (int x = 0; x < 3; ++x) {
                     IRecipeSlotBuilder slot = recipeLayout.addSlot(RecipeIngredientRole.INPUT, x * 18 + 1, y * 18 + 1);
-                    if (x >= recipe.getWidth()) {
+                    if (x >= recipe.getWidth() || y >= recipe.getHeight()) {
                         //Outside height, insert air.
                         slot.addIngredient(VanillaTypes.ITEM, ItemStack.EMPTY);
                         continue;
