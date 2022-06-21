38d80221c6679f137ab5ee6d7ca24fd0c1d00cb4
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2022-06-20 23:11:07 -0400

    removed fabricated forge modules and replaced them with jitpack deps

--- fabricated-forge-datagen/src/main/java/net/minecraftforge/client/model/generators/IGeneratedBlockstate.java
+++
@@ -1,9 +0,0 @@
-package net.minecraftforge.client.model.generators;
-
-import com.google.common.annotations.VisibleForTesting;
-import com.google.gson.JsonObject;
-
-@VisibleForTesting
-public interface IGeneratedBlockstate {
-    JsonObject toJson();
-}
