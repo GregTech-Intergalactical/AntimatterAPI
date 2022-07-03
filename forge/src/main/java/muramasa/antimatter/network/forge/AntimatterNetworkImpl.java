8c6f0173c18e7b51f4b5cf11ce5d3a559e24c4b2
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2022-07-03 18:53:04 -0400

    little work on network

--- forge/src/main/java/muramasa/antimatter/network/forge/AntimatterNetworkImpl.java
+++ forge/src/main/java/muramasa/antimatter/network/forge/AntimatterNetworkImpl.java
@@ -5,14 +5,22 @@ import muramasa.antimatter.network.AntimatterNetwork;
 import muramasa.antimatter.network.packets.CoverGuiEventPacket;
 import muramasa.antimatter.network.packets.GuiSyncPacket;
 import muramasa.antimatter.network.packets.TileGuiEventPacket;
+import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.common.util.FakePlayer;
 import net.minecraftforge.network.NetworkDirection;
+import net.minecraftforge.network.NetworkEvent;
 import net.minecraftforge.network.NetworkRegistry;
+import net.minecraftforge.network.simple.IndexedMessageCodec;
 import net.minecraftforge.network.simple.SimpleChannel;
 import net.minecraftforge.server.ServerLifecycleHooks;
+
+import java.util.Optional;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class AntimatterNetworkImpl extends AntimatterNetwork {
     private SimpleChannel handler;
@@ -55,6 +63,10 @@ public class AntimatterNetworkImpl extends AntimatterNetwork {
             }
             ctx.get().setPacketHandled(true);
         });
+    }
+
+    public <MSG> void registerMessage(Class<MSG> messageType, BiConsumer<MSG, FriendlyByteBuf> encoder, Function<FriendlyByteBuf, MSG> decoder, BiConsumer<MSG, Supplier<NetworkEvent.Context>> messageConsumer) {
+        handler.registerMessage(currMessageId++, messageType, encoder, decoder, messageConsumer);
     }
 
     public void sendToServer(Object msg) {
