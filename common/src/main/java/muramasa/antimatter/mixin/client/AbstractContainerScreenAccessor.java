010213fdd5f9bae057f88f2e9e7a951254cd8494
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2022-07-04 01:23:50 -0400

    added couple more methods to abstract container screen mixin

--- common/src/main/java/muramasa/antimatter/mixin/client/AbstractContainerScreenAccessor.java
+++ common/src/main/java/muramasa/antimatter/mixin/client/AbstractContainerScreenAccessor.java
@@ -5,10 +5,16 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(AbstractContainerScreen.class)
-public interface AbstractContainerAccessor {
+public interface AbstractContainerScreenAccessor {
     @Accessor
     int getImageWidth();
 
     @Accessor
     int getImageHeight();
+
+    @Accessor
+    int getTopPos();
+
+    @Accessor
+    int getLeftPos();
 }
