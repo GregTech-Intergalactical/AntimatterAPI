c9e2436f008bfeb790b67dda4841029721c7ee66
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2022-07-19 00:38:38 -0400

    fixed refmaps

---
+++ fabric/src/main/java/muramasa/antimatter/fluid/fabric/AntimatterFluidUtilsImpl.java
@@ -0,0 +1,16 @@
+package muramasa.antimatter.fluid.fabric;
+
+import muramasa.antimatter.fluid.AntimatterFluid;
+import net.minecraft.world.level.material.FlowingFluid;
+import net.minecraft.world.level.material.Fluid;
+
+public class AntimatterFluidUtilsImpl {
+    //TODO
+    public static Fluid createSourceFluid(AntimatterFluid fluid){
+        return null;
+    }
+
+    public static FlowingFluid createFlowingFluid(AntimatterFluid fluid){
+        return null;
+    }
+}
