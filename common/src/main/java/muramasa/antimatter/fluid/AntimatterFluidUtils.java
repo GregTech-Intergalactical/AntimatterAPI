932a4ad9bdfc0d69ec5299fcd01bfd7c46313628
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2022-07-21 00:19:17 -0400

    woops forgot to add expect platform above new method

--- common/src/main/java/muramasa/antimatter/fluid/AntimatterFluidUtils.java
+++ common/src/main/java/muramasa/antimatter/fluid/AntimatterFluidUtils.java
@@ -7,17 +7,8 @@ import net.minecraft.world.level.material.Fluid;
 import java.util.function.Consumer;
 
 public class AntimatterFluidUtils {
+    @ExpectPlatform
     public static void createSourceAndFlowingFluid(AntimatterFluid fluid, Consumer<Fluid> source, Consumer<FlowingFluid> flowing){
-        throw new AssertionError();
-    }
-
-    @ExpectPlatform
-    public static Fluid createSourceFluid(AntimatterFluid fluid){
-        throw new AssertionError();
-    }
-
-    @ExpectPlatform
-    public static FlowingFluid createFlowingFluid(AntimatterFluid fluid){
         throw new AssertionError();
     }
 }
