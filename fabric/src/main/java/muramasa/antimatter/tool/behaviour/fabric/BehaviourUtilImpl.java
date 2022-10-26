1abb4bd3bb039cdd442450cc3f860e2b26776519
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2022-10-26 17:30:00 -0400

    added todo comment

--- fabric/src/main/java/muramasa/antimatter/tool/behaviour/fabric/BehaviourUtilImpl.java
+++ fabric/src/main/java/muramasa/antimatter/tool/behaviour/fabric/BehaviourUtilImpl.java
@@ -9,6 +9,7 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class BehaviourUtilImpl {
+    //TODO make this use fabric events, if they exist
     public static BlockState onToolUse(BlockState originalState, Level world, BlockPos pos, Player player, ItemStack stack, String action){
         return originalState;
     }
