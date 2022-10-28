a6befd2b9f7c24b1496c15ef5dff5435edce6bcf
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2022-10-28 00:12:49 -0400

    fixed forge isModLoaded when run too early

--- forge/src/main/java/muramasa/antimatter/forge/AntimatterAPIImpl.java
+++ forge/src/main/java/muramasa/antimatter/forge/AntimatterAPIImpl.java
@@ -4,11 +4,16 @@ import muramasa.antimatter.registration.forge.AntimatterRegistration;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.LoadingModList;
+import net.minecraftforge.fml.loading.moddiscovery.ModInfo;
 import net.minecraftforge.registries.IForgeRegistryEntry;
 
 public class AntimatterAPIImpl {
     public static void registerTransferApi(BlockEntityType<?> type){}
     public static boolean isModLoaded(String mod) {
+        if (ModList.get() == null) {
+            return LoadingModList.get().getMods().stream().map(ModInfo::getModId).anyMatch(mod::equals);
+        }
         return ModList.get().isLoaded(mod);
     }
 
