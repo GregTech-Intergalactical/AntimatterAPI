d97a85d0a805b2adb504223872d1b7a3e9a7e136
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2022-10-28 23:57:41 -0400

    fixed compile error with forge rei plugin

--- forge/src/main/java/muramasa/antimatter/integration/rei/forge/AntimatterREIPluginForge.java
+++ forge/src/main/java/muramasa/antimatter/integration/rei/forge/AntimatterREIPluginForge.java
@@ -1,9 +1,9 @@
 package muramasa.antimatter.integration.rei.forge;
 
-import me.shedaniel.rei.forge.REIPlugin;
+import me.shedaniel.rei.forge.REIPluginClient;
 import muramasa.antimatter.integration.rei.AntimatterREIPlugin;
 import net.minecraftforge.api.distmarker.Dist;
 
-@REIPlugin(Dist.CLIENT)
+@REIPluginClient()
 public class AntimatterREIPluginForge extends AntimatterREIPlugin {
 }
