24ce137b26f008e8b68b4b90c28601b8d8620f9c
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2022-11-02 16:36:00 -0400

    fixed syncing of cover modes

--- common/src/main/java/muramasa/antimatter/cover/ICoverModeHandler.java
+++ common/src/main/java/muramasa/antimatter/cover/ICoverModeHandler.java
@@ -2,6 +2,9 @@ package muramasa.antimatter.cover;
 
 public interface ICoverModeHandler {
     ICoverMode getCoverMode();
+     int coverModeToInt();
+
+     void setCoverMode(int index);
 
     default int getOverlayX() {
         return 176;
--- common/src/main/java/muramasa/antimatter/gui/widget/CoverModeHandlerWidget.java
+++ common/src/main/java/muramasa/antimatter/gui/widget/CoverModeHandlerWidget.java
@@ -4,6 +4,7 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import muramasa.antimatter.cover.ICoverMode;
 import muramasa.antimatter.cover.ICoverModeHandler;
 import muramasa.antimatter.gui.GuiInstance;
+import muramasa.antimatter.gui.ICanSyncData;
 import muramasa.antimatter.gui.IGuiElement;
 import muramasa.antimatter.gui.Widget;
 import org.jetbrains.annotations.NotNull;
@@ -12,6 +13,15 @@ import org.jetbrains.annotations.Nullable;
 public class CoverModeHandlerWidget  extends Widget {
     public CoverModeHandlerWidget(@NotNull GuiInstance gui, @Nullable IGuiElement parent) {
         super(gui, parent);
+    }
+
+    @Override
+    public void init() {
+        super.init();
+        if (gui.handler instanceof ICoverModeHandler coverModeHandler){
+            gui.syncInt(coverModeHandler::coverModeToInt, coverModeHandler::setCoverMode, ICanSyncData.SyncDirection.SERVER_TO_CLIENT);
+        }
+
     }
 
     @Override
