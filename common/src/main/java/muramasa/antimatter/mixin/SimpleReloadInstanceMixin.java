03a388ac922ab8a6a05ec8f95a8c767ef83ba4b5
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2022-11-19 22:51:48 -0500

    fixed kubejs recipe compat

--- common/src/main/java/muramasa/antimatter/mixin/SimpleReloadInstanceMixin.java
+++ common/src/main/java/muramasa/antimatter/mixin/SimpleReloadInstanceMixin.java
@@ -4,6 +4,7 @@ import muramasa.antimatter.datagen.AntimatterDynamics;
 import net.minecraft.commands.Commands;
 import net.minecraft.core.RegistryAccess;
 import net.minecraft.server.ReloadableServerResources;
+import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -17,7 +18,7 @@ import java.util.concurrent.Executor;
 @Mixin(ReloadableServerResources.class)
 public class SimpleReloadInstanceMixin {
     
-    @Inject(method = "loadResources", at = @At(value = "HEAD"))
+    @Inject(method = "loadResources", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/packs/resources/SimpleReloadInstance;create(Lnet/minecraft/server/packs/resources/ResourceManager;Ljava/util/List;Ljava/util/concurrent/Executor;Ljava/util/concurrent/Executor;Ljava/util/concurrent/CompletableFuture;Z)Lnet/minecraft/server/packs/resources/ReloadInstance;", shift = At.Shift.BEFORE))
     private static void create(ResourceManager p_206862_, RegistryAccess.Frozen p_206863_, Commands.CommandSelection p_206864_, int p_206865_, Executor p_206866_, Executor p_206867_, CallbackInfoReturnable<CompletableFuture<ReloadableServerResources>> ri) {
         AntimatterDynamics.onResourceReload(p_206864_ != Commands.CommandSelection.INTEGRATED);
     }
