2943a4de431297ca5f142b54436249ac23a10ade
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2022-12-07 21:18:34 -0500

    temp fix for recipe datagen

--- common/src/main/java/muramasa/antimatter/datagen/AntimatterDynamics.java
+++ common/src/main/java/muramasa/antimatter/datagen/AntimatterDynamics.java
@@ -93,6 +93,8 @@ public class AntimatterDynamics {
         Stream<IAntimatterProvider> sync = providers.stream().filter(t -> !t.async());
         Stream.concat(async, sync).forEach(IAntimatterProvider::run);
         providers.forEach(IAntimatterProvider::onCompletion);
+        //TODO fix poroperly
+        collectRecipes(rec -> DYNAMIC_RECIPES.addData(fix(rec.getId(), "recipes", "json"), rec.serializeRecipe().toString().getBytes()));
         Antimatter.LOGGER.info("Time to run data providers: " + (System.currentTimeMillis() - time) + " ms.");
         if (!AntimatterPlatformUtils.isProduction() && exportPack) {
             DYNAMIC_RESOURCE_PACK.dump(new File("./dumped"));
