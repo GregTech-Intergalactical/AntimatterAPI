bc8f777dcc931b71c6823124e03af52fea779c93
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2022-12-15 00:46:04 -0500

    started work on magane compat

---
+++ common/src/main/java/muramasa/antimatter/integration/megane/AntimatterMeganeModule.java
@@ -0,0 +1,23 @@
+package muramasa.antimatter.integration.megane;
+
+import lol.bai.megane.api.MeganeModule;
+import lol.bai.megane.api.registry.ClientRegistrar;
+import lol.bai.megane.api.registry.CommonRegistrar;
+import muramasa.antimatter.Ref;
+import muramasa.antimatter.integration.megane.provider.MachineEnergyProvider;
+import muramasa.antimatter.integration.megane.provider.MachineProgressProvider;
+import muramasa.antimatter.tile.TileEntityMachine;
+
+public class AntimatterMeganeModule implements MeganeModule {
+
+    @Override
+    public void registerCommon(CommonRegistrar registrar) {
+        registrar.addEnergy(TileEntityMachine.class, new MachineEnergyProvider());
+        registrar.addProgress(TileEntityMachine.class, new MachineProgressProvider());
+    }
+
+    @Override
+    public void registerClient(ClientRegistrar registrar) {
+        registrar.addEnergyInfo(Ref.ID, 0xEEE600, "EU");
+    }
+}
---
+++ common/src/main/java/muramasa/antimatter/integration/megane/provider/MachineProgressProvider.java
@@ -0,0 +1,37 @@
+package muramasa.antimatter.integration.megane.provider;
+
+import lol.bai.megane.api.provider.ProgressProvider;
+import muramasa.antimatter.tile.TileEntityMachine;
+import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+public class MachineProgressProvider extends ProgressProvider<TileEntityMachine> {
+    @Override
+    public int getInputSlotCount() {
+        return getCastedObject().itemHandler.map(i -> i.getInputHandler().getSlots()).orElse(0);
+    }
+
+    @Override
+    public int getOutputSlotCount() {
+        return getCastedObject().itemHandler.map(i -> i.getOutputHandler().getSlots()).orElse(0);
+    }
+
+    @Override
+    public @NotNull ItemStack getInputStack(int slot) {
+        return getCastedObject().itemHandler.map(i -> i.getInputHandler().getStackInSlot(slot)).orElse(ItemStack.EMPTY);
+    }
+
+    @Override
+    public @NotNull ItemStack getOutputStack(int slot) {
+        return getCastedObject().itemHandler.map(i -> i.getOutputHandler().getStackInSlot(slot)).orElse(ItemStack.EMPTY);
+    }
+
+    @Override
+    public int getPercentage() {
+        return getCastedObject().recipeHandler.map(r -> ((float)r.getCurrentProgress() / (float) r.getMaxProgress()) * 100).orElse(0f).intValue();
+    }
+
+    TileEntityMachine<?> getCastedObject(){
+        return (TileEntityMachine<?>) getObject();
+    }
+}
