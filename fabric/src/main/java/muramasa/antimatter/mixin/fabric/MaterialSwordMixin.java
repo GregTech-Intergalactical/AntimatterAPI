dd957d8969a5ec83853250d7bb3442f6e5b8af8c
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-01-03 23:52:21 -0500

    fixed compile error

--- fabric/src/main/java/muramasa/antimatter/mixin/fabric/MaterialSwordMixin.java
+++ fabric/src/main/java/muramasa/antimatter/mixin/fabric/MaterialSwordMixin.java
@@ -1,11 +1,11 @@
 package muramasa.antimatter.mixin.fabric;
 
 import io.github.fabricators_of_create.porting_lib.util.DamageableItem;
-import io.github.feltmc.feltapi.api.item.extensions.EnchantabilityItem;
-import io.github.feltmc.feltapi.api.item.extensions.SneakBypassUseItem;
+import io.github.feltmc.feltapi.api.enchanting.EnchantabilityItem;
+import io.github.feltmc.feltapi.api.playeritem.SneakBypassUseItem;
 import muramasa.antimatter.tool.MaterialSword;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(value = MaterialSword.class, remap = false)
-public class MaterialSwordMixin implements EnchantabilityItem, DamageableItem, SneakBypassUseItem {
+public abstract class MaterialSwordMixin implements EnchantabilityItem, DamageableItem, SneakBypassUseItem {
 }
--- fabric/src/main/java/muramasa/antimatter/mixin/fabric/MaterialToolMixin.java
+++ fabric/src/main/java/muramasa/antimatter/mixin/fabric/MaterialToolMixin.java
@@ -3,11 +3,11 @@ package muramasa.antimatter.mixin.fabric;
 import io.github.fabricators_of_create.porting_lib.item.ReequipAnimationItem;
 import io.github.fabricators_of_create.porting_lib.util.DamageableItem;
 import io.github.fabricators_of_create.porting_lib.util.ShieldBlockItem;
-import io.github.feltmc.feltapi.api.item.extensions.EnchantabilityItem;
-import io.github.feltmc.feltapi.api.item.extensions.SneakBypassUseItem;
+import io.github.feltmc.feltapi.api.enchanting.EnchantabilityItem;
+import io.github.feltmc.feltapi.api.playeritem.SneakBypassUseItem;
 import muramasa.antimatter.tool.MaterialTool;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(value = MaterialTool.class, remap = false)
-public class MaterialToolMixin implements DamageableItem, EnchantabilityItem, ReequipAnimationItem, ShieldBlockItem, SneakBypassUseItem {
+public abstract class MaterialToolMixin implements DamageableItem, EnchantabilityItem, ReequipAnimationItem, ShieldBlockItem, SneakBypassUseItem {
 }
