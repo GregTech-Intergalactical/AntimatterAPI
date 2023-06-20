5fa2c30aef2486fd72fb8f195bded3923a2fca48
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-06-20 19:35:35 -0400

    made pure dust washable in the cauldron

--- common/src/main/java/muramasa/antimatter/item/interaction/CauldronInteractions.java
+++ common/src/main/java/muramasa/antimatter/item/interaction/CauldronInteractions.java
@@ -14,6 +14,7 @@ public class CauldronInteractions {
 
     public static void init() {
         DUST_IMPURE.all().stream().filter(t -> t.has(DUST)).map(t -> DUST_IMPURE.get(t)).forEach(stack -> CauldronInteraction.WATER.put(stack, MaterialItem::interactWithCauldron));
+        DUST_PURE.all().stream().filter(t -> t.has(DUST)).map(t -> DUST_PURE.get(t)).forEach(stack -> CauldronInteraction.WATER.put(stack, MaterialItem::interactWithCauldron));
         CRUSHED.all().stream().filter(t -> t.has(CRUSHED_PURIFIED) && t.has(DUST)).map(t -> CRUSHED.get(t)).forEach(stack -> CauldronInteraction.WATER.put(stack, MaterialItem::interactWithCauldron));
         AntimatterAPI.all(ItemFluidCell.class, t -> CauldronInteraction.WATER.put(t, ItemFluidCell::interactWithCauldron));
     }
