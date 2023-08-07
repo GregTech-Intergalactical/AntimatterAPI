3497bf9da17517218989b8a8d4ea1f6eb097c452
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-08-07 12:58:18 -0400

    added wrench and wire cutter info to the one probe tool info

--- common/src/main/java/muramasa/antimatter/integration/top/TheOneProbePlugin.java
+++ common/src/main/java/muramasa/antimatter/integration/top/TheOneProbePlugin.java
@@ -1,12 +1,17 @@
 package muramasa.antimatter.integration.top;
 
 import mcjty.theoneprobe.api.ITheOneProbe;
+import mcjty.theoneprobe.config.Config;
+import muramasa.antimatter.Ref;
+import net.minecraft.resources.ResourceLocation;
 
 import java.util.function.Function;
 
 public class TheOneProbePlugin implements Function<ITheOneProbe, Void> {
 	@Override
 	public Void apply(ITheOneProbe input) {
+		Config.getTooltypeTags().putIfAbsent(new ResourceLocation(Ref.ID, "mineable/wrench"), "Wrench");
+		Config.getTooltypeTags().putIfAbsent(new ResourceLocation(Ref.ID, "mineable/wire_cutter"), "Wire Cutter");
 		input.registerProvider(new EnergyInfoProvider());
 		input.registerProvider(new MultiblockInfoProvider());
 		input.registerProvider(new RecipeInfoProvider());
