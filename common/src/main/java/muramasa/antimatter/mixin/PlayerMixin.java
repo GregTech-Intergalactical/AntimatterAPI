816fe53c45e8bc85e720afb9e839c19b8d8ca9ee
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-08-31 19:11:54 -0400

    finished mixin required for Implosion enchantment

--- common/src/main/java/muramasa/antimatter/mixin/PlayerMixin.java
+++ common/src/main/java/muramasa/antimatter/mixin/PlayerMixin.java
@@ -1,4 +1,27 @@
 package muramasa.antimatter.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import muramasa.antimatter.Data;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.MobType;
+import net.minecraft.world.entity.monster.Creeper;
+import net.minecraft.world.entity.monster.Ghast;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(Player.class)
 public class PlayerMixin {
+    @WrapOperation(method = "attack", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/enchantment/EnchantmentHelper;getDamageBonus(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/entity/MobType;)F"))
+    private float redirectDamageBonus(ItemStack stack, MobType type, Operation<Float> original, Entity target){
+        if (target instanceof LivingEntity living){
+            if (living instanceof Creeper || living instanceof Ghast){
+                type = Data.CREEPER;
+            }
+        }
+        return original.call(stack, type);
+    }
 }
