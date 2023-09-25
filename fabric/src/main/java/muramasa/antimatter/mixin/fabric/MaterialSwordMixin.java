3a930ae3793accbdee91a6f2d6c0fd298dadee30
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-09-25 19:17:23 -0400

    changed to new felt api path

--- fabric/src/main/java/muramasa/antimatter/mixin/fabric/MaterialSwordMixin.java
+++ fabric/src/main/java/muramasa/antimatter/mixin/fabric/MaterialSwordMixin.java
@@ -1,9 +1,9 @@
 package muramasa.antimatter.mixin.fabric;
 
 import io.github.fabricators_of_create.porting_lib.util.DamageableItem;
-import io.github.feltmc.feltapi.api.enchanting.EnchantabilityItem;
-import io.github.feltmc.feltapi.api.playeritem.SneakBypassUseItem;
 import muramasa.antimatter.tool.MaterialSword;
+import net.feltmc.feltapi.api.enchanting.EnchantabilityItem;
+import net.feltmc.feltapi.api.playeritem.SneakBypassUseItem;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(value = MaterialSword.class, remap = false)
--- fabric/src/main/java/muramasa/antimatter/mixin/fabric/MaterialToolMixin.java
+++ fabric/src/main/java/muramasa/antimatter/mixin/fabric/MaterialToolMixin.java
@@ -3,9 +3,9 @@ package muramasa.antimatter.mixin.fabric;
 import io.github.fabricators_of_create.porting_lib.item.ReequipAnimationItem;
 import io.github.fabricators_of_create.porting_lib.util.DamageableItem;
 import io.github.fabricators_of_create.porting_lib.util.ShieldBlockItem;
-import io.github.feltmc.feltapi.api.enchanting.EnchantabilityItem;
-import io.github.feltmc.feltapi.api.playeritem.SneakBypassUseItem;
 import muramasa.antimatter.tool.MaterialTool;
+import net.feltmc.feltapi.api.enchanting.EnchantabilityItem;
+import net.feltmc.feltapi.api.playeritem.SneakBypassUseItem;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(value = MaterialTool.class, remap = false)
