610a039b89d79224d1bb031a9b41afe463c28cba
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-10-06 00:33:25 -0400

    fixedpipe covers not getting nbt on place when said cover is first cover placed on pipe

--- common/src/main/java/muramasa/antimatter/capability/pipe/PipeCoverHandler.java
+++ common/src/main/java/muramasa/antimatter/capability/pipe/PipeCoverHandler.java
@@ -4,6 +4,8 @@ import muramasa.antimatter.capability.CoverHandler;
 import muramasa.antimatter.cover.ICover;
 import muramasa.antimatter.blockentity.pipe.BlockEntityPipe;
 import net.minecraft.core.Direction;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 
 public class PipeCoverHandler<T extends BlockEntityPipe<?>> extends CoverHandler<T> {
 
@@ -12,7 +14,7 @@ public class PipeCoverHandler<T extends BlockEntityPipe<?>> extends CoverHandler
         // if (tag != null) deserialize(tag);
     }
 
-    @Override
+    /*@Override
     public boolean set(Direction side, ICover old, ICover stack, boolean sync) {
         boolean ok = super.set(side, old, stack, sync);
         if (ok && sync) {
@@ -20,8 +22,30 @@ public class PipeCoverHandler<T extends BlockEntityPipe<?>> extends CoverHandler
             this.getTile().onCoverUpdate(!old.isEmpty() && stack.isEmpty(), anyEmpty, side, old, stack);
         }
         return ok;
+    }*/
+
+    @Override
+    public boolean placeCover(Player player, Direction side, ItemStack stack, ICover cover) {
+        ICover old = get(side);
+        boolean ok = super.placeCover(player, side, stack, cover);
+        if (ok){
+            boolean anyEmpty = this.covers.values().stream().anyMatch(t -> !t.isEmpty());
+            this.getTile().onCoverUpdate(false, anyEmpty, side, old, cover);
+        }
+        return ok;
     }
 
+    @Override
+    public boolean removeCover(Player player, Direction side, boolean onlyRemove) {
+        ICover old = get(side);
+        boolean ok = super.removeCover(player, side, onlyRemove);
+        if (ok){
+            ICover stack = ICover.empty;
+            boolean anyEmpty = this.covers.values().stream().anyMatch(t -> !t.isEmpty());
+            this.getTile().onCoverUpdate(true, anyEmpty, side, old, stack);
+        }
+        return ok;
+    }
 
     public boolean onTransfer(Object obj, Direction from, Direction towards, boolean simulate) {
         return super.onTransfer(obj, from, towards, simulate) || this.get(towards).onTransfer(obj, false, simulate);
