b737a72e5ce7bb77cc672fde1f6655e934ee4a7d
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2023-10-23 21:39:02 -0400

    made water have the forge:water tag

--- common/src/main/java/muramasa/antimatter/datagen/providers/AntimatterFluidTagProvider.java
+++ common/src/main/java/muramasa/antimatter/datagen/providers/AntimatterFluidTagProvider.java
@@ -1,6 +1,7 @@
 package muramasa.antimatter.datagen.providers;
 
 import muramasa.antimatter.AntimatterAPI;
+import muramasa.antimatter.Ref;
 import muramasa.antimatter.data.AntimatterTags;
 import muramasa.antimatter.datagen.IAntimatterProvider;
 import muramasa.antimatter.fluid.AntimatterFluid;
@@ -9,6 +10,7 @@ import muramasa.antimatter.material.Material;
 import muramasa.antimatter.material.MaterialTags;
 import net.minecraft.core.Registry;
 import net.minecraft.world.level.material.Fluid;
+import net.minecraft.world.level.material.Fluids;
 
 import static muramasa.antimatter.util.TagUtils.getForgelikeFluidTag;
 
@@ -36,5 +38,8 @@ public class AntimatterFluidTagProvider extends AntimatterTagProvider<Fluid> imp
                 }
             }
         });
+        if (domain.equals(Ref.ID)){
+            tag(getForgelikeFluidTag("water")).add(Fluids.WATER);
+        }
     }
 }
