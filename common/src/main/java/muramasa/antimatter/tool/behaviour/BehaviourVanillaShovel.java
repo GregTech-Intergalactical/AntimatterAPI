43992537b939c7c5e886c61675edcbda3437906e
John Wheatfield <rchpweblo@gmail.com>
2024-04-30 01:53:30 -0700

    Rebuilt shovel code to be dynamic as to allow integration of new path types.

--- common/src/main/java/muramasa/antimatter/tool/behaviour/BehaviourVanillaShovel.java
+++ common/src/main/java/muramasa/antimatter/tool/behaviour/BehaviourVanillaShovel.java
@@ -1,7 +1,8 @@
 package muramasa.antimatter.tool.behaviour;
 
+import com.google.common.collect.ImmutableMap;
+import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import muramasa.antimatter.behaviour.IItemUse;
-import muramasa.antimatter.tool.IAntimatterTool;
 import muramasa.antimatter.tool.IBasicAntimatterTool;
 import muramasa.antimatter.util.Utils;
 import net.minecraft.core.BlockPos;
@@ -14,13 +15,23 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.CampfireBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.Property;
 
 public class BehaviourVanillaShovel implements IItemUse<IBasicAntimatterTool> {
 
     public static final BehaviourVanillaShovel INSTANCE = new BehaviourVanillaShovel();
+
+    //Adding this to allow new path blocks to be dynamically added via integration. Vanilla contains only one block and the campfires.
+    private static final Object2ObjectOpenHashMap<Block, Block> FLATTENING_MAP = new Object2ObjectOpenHashMap<>();
+
+    static {
+        new ImmutableMap.Builder<Block, Block>().put(Blocks.GRASS_BLOCK, Blocks.DIRT_PATH).put(Blocks.CAMPFIRE, Blocks.CAMPFIRE).put(Blocks.SOUL_CAMPFIRE, Blocks.SOUL_CAMPFIRE)
+                .build().forEach(BehaviourVanillaShovel::addStrippedBlock);
+    }
 
     @Override
     public String getId() {
@@ -32,16 +43,17 @@ public class BehaviourVanillaShovel implements IItemUse<IBasicAntimatterTool> {
         if (c.getClickedFace() == Direction.DOWN) return InteractionResult.PASS;
         BlockState state = c.getLevel().getBlockState(c.getClickedPos());
         BlockState changedState = null;
-        if (state.getBlock() == Blocks.GRASS_BLOCK && c.getLevel().isEmptyBlock(c.getClickedPos().above())) {
-            changedState = getToolModifiedState(state, Blocks.DIRT_PATH.defaultBlockState(), c.getLevel(), c.getClickedPos(), c.getPlayer(), c.getItemInHand(), "shovel_flatten");
+        if (state.getBlock() instanceof CampfireBlock && state.getValue(CampfireBlock.LIT)) {
+            state.setValue(CampfireBlock.LIT, false);
+            changedState = getToolModifiedState(state, c.getLevel(), c.getClickedPos(), c.getPlayer(), c.getItemInHand(), "shovel_dig");
+            if (changedState != null) {
+                c.getLevel().levelEvent(c.getPlayer(), 1009, c.getClickedPos(), 0);
+            }
+        } else if (state != null) {
+            changedState = getToolModifiedState(state, c.getLevel(), c.getClickedPos(), c.getPlayer(), c.getItemInHand(), "shovel_flatten");
             if (changedState != null) {
                 SoundEvent soundEvent = instance.getAntimatterToolType().getUseSound() == null ? SoundEvents.SHOVEL_FLATTEN : instance.getAntimatterToolType().getUseSound();
                 c.getLevel().playSound(c.getPlayer(), c.getClickedPos(), soundEvent, SoundSource.BLOCKS, 1.0F, 1.0F);
-            }
-        } else if (state.getBlock() instanceof CampfireBlock && state.getValue(CampfireBlock.LIT)) {
-            changedState = getToolModifiedState(state, state.setValue(CampfireBlock.LIT, false), c.getLevel(), c.getClickedPos(), c.getPlayer(), c.getItemInHand(), "shovel_dig");
-            if (changedState != null) {
-                c.getLevel().levelEvent(c.getPlayer(), 1009, c.getClickedPos(), 0);
             }
         }
         if (changedState != null) {
@@ -51,8 +63,19 @@ public class BehaviourVanillaShovel implements IItemUse<IBasicAntimatterTool> {
         } else return InteractionResult.PASS;
     }
 
-    private BlockState getToolModifiedState(BlockState originalState, BlockState changedState, Level world, BlockPos pos, Player player, ItemStack stack, String action) {
+    private BlockState getToolModifiedState(BlockState originalState, Level world, BlockPos pos, Player player, ItemStack stack, String action) {
         BlockState eventState = BehaviourUtil.onToolUse(originalState, world, pos, player, stack, action);
-        return eventState != originalState ? eventState : changedState;
+        if (eventState != originalState) return eventState;
+        Block flattened = FLATTENING_MAP.get(originalState.getBlock());
+        if (flattened == null) return null;
+        BlockState state = flattened.defaultBlockState();
+        for (Property property : originalState.getProperties()) {
+            if (state.hasProperty(property)){
+                state = state.setValue(property, originalState.getValue(property));
+            }
+        }
+        return state;
     }
+
+    public static void addStrippedBlock(Block from, Block to) { FLATTENING_MAP.put(from, to); }
 }
