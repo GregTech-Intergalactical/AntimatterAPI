5af2eb17249c433cd58baa7c2092ad457f5a858b
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2024-05-12 10:19:19 -0400

    fixed AntimatterTagBuilder

--- common/src/main/java/muramasa/antimatter/datagen/builder/AntimatterTagBuilder.java
+++ common/src/main/java/muramasa/antimatter/datagen/builder/AntimatterTagBuilder.java
@@ -1,5 +1,7 @@
 package muramasa.antimatter.datagen.builder;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceKey;
@@ -7,14 +9,16 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagBuilder;
 import net.minecraft.tags.TagEntry;
 import net.minecraft.tags.TagKey;
+import net.minecraft.util.GsonHelper;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Stream;
 
 public class AntimatterTagBuilder<T> {
-    public final TagBuilder builder;
+    public TagBuilder builder;
     public final Registry<T> registry;
     public final List<T> removeElements = new ArrayList<>();
     private final String source;
@@ -44,6 +48,13 @@ public class AntimatterTagBuilder<T> {
         return this;
     }
 
+    public AntimatterTagBuilder<T> add(ResourceLocation... ids) {
+        for(ResourceLocation id : ids) {
+            this.builder.addElement(id);
+        }
+        return this;
+    }
+
     public AntimatterTagBuilder<T> addOptional(ResourceLocation location) {
         this.builder.addOptionalElement(location);
         return this;
@@ -51,6 +62,11 @@ public class AntimatterTagBuilder<T> {
 
     public AntimatterTagBuilder<T> addTag(TagKey<T> tag) {
         this.builder.addTag(tag.location());
+        return this;
+    }
+
+    public AntimatterTagBuilder<T> addTag(ResourceLocation tag){
+        this.builder.addTag(tag);
         return this;
     }
 
@@ -81,13 +97,43 @@ public class AntimatterTagBuilder<T> {
     }
 
     public AntimatterTagBuilder<T> addFromJson(JsonObject json, String source) {
-        builder.addFromJson(json, source);
+        if (json.get("replace").getAsBoolean()) {
+            builder = new TagBuilder();
+        }
+        JsonArray array = json.getAsJsonArray("values");
+        if (!array.isEmpty()) {
+            for (JsonElement element : array) {
+                if (element.isJsonPrimitive()){
+                    String entry = element.getAsString();
+                    ResourceLocation id = new ResourceLocation(entry.replace("#", "").replace("?", ""));
+                    if (entry.startsWith("#")) {
+                        if (entry.endsWith("?")) {
+                            addOptionalTag(id);
+                        } else {
+                            addTag(id);
+                        }
+                    } else {
+                        if (entry.endsWith("?")) {
+                            addOptional(id);
+                        } else {
+                            add(id);
+                        }
+                    }
+                }
+            }
+        }
         return this;
     }
 
     public JsonObject serializeToJson() {
-        JsonObject jsonObject = builder.serializeToJson();
+        JsonObject jsonObject = new JsonObject();
+        JsonArray jsonArray = new JsonArray();
+        for (TagEntry entry : builder.build()){
+            jsonArray.add(entry.toString());
+        }
+
         jsonObject.addProperty("replace", replace);
+        jsonObject.add("values", jsonArray);
         return jsonObject;
     }
 }
