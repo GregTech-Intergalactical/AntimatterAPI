4de45d17b6b5abe95a918a9d3224b4733b8b4884
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2024-05-12 07:43:52 -0400

    changed order of some stuff in recipe schema

--- common/src/main/java/muramasa/antimatter/integration/kubejs/MachineRecipeSchema.java
+++ common/src/main/java/muramasa/antimatter/integration/kubejs/MachineRecipeSchema.java
@@ -18,17 +18,20 @@ public interface MachineRecipeSchema {
     });
     RecipeKey<String> MAP = MAP_COMPONENT.key("map");
     RecipeKey<InputItem[]> INPUT_ITEMS = ItemComponents.INPUT_ARRAY.key("inputItems");
-    RecipeKey<OutputItem[]> OUTPUT_ITEMS = ItemComponents.OUTPUT_ARRAY.key("outputItems");
     RecipeKey<InputFluid[]> INPUT_FLUIDS = FluidComponents.INPUT_ARRAY.key("inputFluids");
-    RecipeKey<OutputFluid[]> OUTPUT_FLUIDS = FluidComponents.OUTPUT_ARRAY.key("outputFluids");
     RecipeKey<Integer> DURATION = NumberComponent.INT.key("duration");
-    RecipeKey<Long> POWER = NumberComponent.LONG.key("eu");
-    RecipeKey<Integer> AMPS = NumberComponent.INT.key("amps").optional(1);
-    RecipeKey<Integer> SPECIAL = NumberComponent.INT.key("special").optional(0);
-    RecipeKey<Boolean> HIDDEN = BooleanComponent.BOOLEAN.key("hidden").optional(false);
-    RecipeKey<Boolean> FAKE = BooleanComponent.BOOLEAN.key("fake").optional(false);
-    RecipeKey<Integer[]> OUTPUT_CHANCES = NumberComponent.INT.asArray().key("outputChances");
-    RecipeKey<Integer[]> INPUT_CHANCES = NumberComponent.INT.asArray().key("inputChances");
+    RecipeKey<Long> POWER = NumberComponent.LONG.key("eu").optional(0L).preferred("power");
+    RecipeKey<OutputItem[]> OUTPUT_ITEMS = ItemComponents.OUTPUT_ARRAY.key("outputItems").optional((OutputItem[]) null).exclude();
+    RecipeKey<OutputFluid[]> OUTPUT_FLUIDS = FluidComponents.OUTPUT_ARRAY.key("outputFluids").optional((OutputFluid[]) null).exclude();
+    RecipeKey<Integer> AMPS = NumberComponent.INT.key("amps").optional(1).exclude();
+    RecipeKey<Integer> SPECIAL = NumberComponent.INT.key("special").optional(0).exclude();
+    RecipeKey<Boolean> HIDDEN = BooleanComponent.BOOLEAN.key("hidden").optional(false).exclude();
+    RecipeKey<Boolean> FAKE = BooleanComponent.BOOLEAN.key("fake").optional(false).exclude();
+    RecipeKey<Integer[]> OUTPUT_CHANCES = NumberComponent.INT.asArray().key("outputChances").optional((Integer[]) null).exclude();
+    RecipeKey<Integer[]> INPUT_CHANCES = NumberComponent.INT.asArray().key("inputChances").optional((Integer[]) null).exclude();
 
-    RecipeSchema SCHEMA = new RecipeSchema(KubeJSRecipe.class, KubeJSRecipe::new, MAP, INPUT_ITEMS, OUTPUT_ITEMS, INPUT_FLUIDS, OUTPUT_FLUIDS, DURATION, POWER, AMPS, SPECIAL, HIDDEN, FAKE, OUTPUT_CHANCES, INPUT_CHANCES);
+    RecipeSchema SCHEMA = new RecipeSchema(KubeJSRecipe.class, KubeJSRecipe::new,
+            MAP, INPUT_ITEMS, INPUT_FLUIDS, DURATION, POWER, OUTPUT_ITEMS, OUTPUT_FLUIDS, AMPS, SPECIAL, HIDDEN, FAKE, OUTPUT_CHANCES, INPUT_CHANCES)
+            .constructor(MAP, INPUT_ITEMS, DURATION, POWER).constructor(MAP, INPUT_FLUIDS, DURATION, POWER)
+            .constructor(MAP, INPUT_ITEMS, DURATION).constructor(MAP, INPUT_FLUIDS, DURATION);
 }
