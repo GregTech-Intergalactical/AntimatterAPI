d135189a48f7ad64a616c71a27441faf30112f91
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2024-05-19 16:13:24 -0400

    more work on expanding widget, probably al I will do with it for now

--- common/src/main/java/muramasa/antimatter/gui/widget/ExpandingWidget.java
+++ common/src/main/java/muramasa/antimatter/gui/widget/ExpandingWidget.java
@@ -21,40 +21,60 @@ import static muramasa.antimatter.gui.widget.ButtonWidget.*;
 public class ExpandingWidget extends Widget {
     final Predicate<IGuiHandler> syncFunction;
     Consumer<ExpandingWidget> onExpand;
-    final ButtonOverlay closed;
+    final ButtonOverlay icon;
     boolean open = false;
     boolean pressed = false;
     int expansion = 50;
+    int rgbEdgeTop, rgbEdgeBottom, rgbCorner, rgbCenter;
 
-    protected ExpandingWidget(@NotNull GuiInstance gui, @Nullable IGuiElement parent, ButtonOverlay closed, Consumer<ExpandingWidget> onExpand, Predicate<IGuiHandler> syncFunction) {
+    protected ExpandingWidget(@NotNull GuiInstance gui, @Nullable IGuiElement parent, ButtonOverlay icon, Consumer<ExpandingWidget> onExpand, Predicate<IGuiHandler> syncFunction) {
+        this(gui, parent, icon, onExpand, syncFunction, backgroundWhiteEdge, backgroundBlackEdge, backgroundCenter, backgroundCenter);
+    }
+    protected ExpandingWidget(@NotNull GuiInstance gui, @Nullable IGuiElement parent, ButtonOverlay icon, Consumer<ExpandingWidget> onExpand, Predicate<IGuiHandler> syncFunction, int rgbEdgeTop, int rgbEdgeBottom, int rgbCorner, int rgbCenter) {
         super(gui, parent);
         this.syncFunction = syncFunction;
-        this.closed = closed;
+        this.icon = icon;
         this.onExpand = onExpand;
+        this.rgbEdgeTop = rgbEdgeTop;
+        this.rgbEdgeBottom = rgbEdgeBottom;
+        this.rgbCorner = rgbCorner;
+        this.rgbCenter = rgbCenter;
     }
 
     @Override
     public void init() {
         super.init();
         this.gui.syncBoolean(() -> syncFunction.test(gui.handler), b -> {
+            if (b != open){
+                if (b){
+                    setX(getX() - expansion);
+                    setW(getW() + expansion);
+                    setH(getH() + expansion);
+                } else {
+                    setX(getX() + expansion);
+                    setW(getW() - expansion);
+                    setH(getH() - expansion);
+                }
+                updateSize();
+            }
             open = b;
         }, ICanSyncData.SyncDirection.SERVER_TO_CLIENT);
     }
 
     @Override
     public void render(PoseStack matrixStack, double mouseX, double mouseY, float partialTicks) {
-        int extra = open ? 50 : 0;
-        int x = realX() - extra;
+        int x = realX();
         int y = realY();
-        int width = getW() + extra;
-        int height = getH() + extra;
-        fillGradient(matrixStack, x, y, width, height, backgroundCenter, backgroundCenter);
-        fillGradient(matrixStack, x + width, y - 1, 1, 1, backgroundCenter, backgroundCenter);
-        fillGradient(matrixStack, x - 1, y + height, 1, 1, backgroundCenter, backgroundCenter);
-        fillGradient(matrixStack, x - 1, y - 1, width + 1, 1, backgroundWhiteEdge, backgroundWhiteEdge);
-        fillGradient(matrixStack, x - 1, y, 1, height, backgroundWhiteEdge, backgroundWhiteEdge);
-        fillGradient(matrixStack, x, y + height, width + 1, 1, backgroundBlackEdge, backgroundBlackEdge);
-        fillGradient(matrixStack, x + width, y, 1, height, backgroundBlackEdge, backgroundBlackEdge);
+        int width = getW();
+        int height = getH();
+        fillGradient(matrixStack, x, y, width, height, rgbCenter, rgbCenter);
+        fillGradient(matrixStack, x + width, y - 1, 1, 1, rgbCorner, rgbCorner);
+        fillGradient(matrixStack, x - 1, y + height, 1, 1, rgbCorner, rgbCorner);
+        fillGradient(matrixStack, x - 1, y - 1, width + 1, 1, rgbEdgeTop, rgbEdgeTop);
+        fillGradient(matrixStack, x - 1, y, 1, height, rgbEdgeTop, rgbEdgeTop);
+        fillGradient(matrixStack, x, y + height, width + 1, 1, rgbEdgeBottom, rgbEdgeBottom);
+        fillGradient(matrixStack, x + width, y, 1, height, rgbEdgeBottom, rgbEdgeBottom);
+        drawTexture(matrixStack, icon.getTexture(), realX() + 1, realY() + 1, 0, 0, icon.getH(), icon.getW(), icon.getH(), icon.getW());
     }
 
     @Override
@@ -81,7 +101,11 @@ public class ExpandingWidget extends Widget {
         Minecraft.getInstance().getSoundManager().play(SimpleSoundInstance.forUI(SoundEvents.UI_BUTTON_CLICK, 1.0F));
     }
 
-    public static WidgetSupplier build(ButtonOverlay closed, Predicate<IGuiHandler> syncFunction) {
-        return builder((a, b) -> new ExpandingWidget(a, b, closed, but -> but.gui.sendPacket(but.gui.handler.createGuiPacket(new GuiEvents.GuiEvent(GuiEvents.EXPANDING_WIDGET, Screen.hasShiftDown() ? 1 : 0, 0))), syncFunction));
+    public static WidgetSupplier build(ButtonOverlay closed, int id, Predicate<IGuiHandler> syncFunction) {
+        return builder((a, b) -> new ExpandingWidget(a, b, closed, but -> but.gui.sendPacket(but.gui.handler.createGuiPacket(new GuiEvents.GuiEvent(GuiEvents.EXPANDING_WIDGET, Screen.hasShiftDown() ? 1 : 0, id))), syncFunction));
+    }
+
+    public static WidgetSupplier build(ButtonOverlay closed, int id, Predicate<IGuiHandler> syncFunction, int rgbEdgeTop, int rgbEdgeBottom, int rgbCorner, int rgbCenter) {
+        return builder((a, b) -> new ExpandingWidget(a, b, closed, but -> but.gui.sendPacket(but.gui.handler.createGuiPacket(new GuiEvents.GuiEvent(GuiEvents.EXPANDING_WIDGET, Screen.hasShiftDown() ? 1 : 0, id))), syncFunction, rgbEdgeTop, rgbEdgeBottom, rgbCorner, rgbCenter));
     }
 }
