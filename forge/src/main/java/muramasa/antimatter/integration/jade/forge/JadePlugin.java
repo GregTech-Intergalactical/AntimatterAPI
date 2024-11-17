58b20b85c4462e7616a7f4d16d12ddc8c13dab3d
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2024-11-17 12:15:51 -0500

    added jade integration for custom mining tools

--- forge/src/main/java/muramasa/antimatter/integration/jade/forge/JadePlugin.java
+++ forge/src/main/java/muramasa/antimatter/integration/jade/forge/JadePlugin.java
@@ -3,10 +3,17 @@ package muramasa.antimatter.integration.jade.forge;
 import mcp.mobius.waila.api.*;
 import muramasa.antimatter.Ref;
 import muramasa.antimatter.blockentity.BlockEntityMachine;
+import muramasa.antimatter.data.AntimatterDefaultTools;
+import muramasa.antimatter.data.AntimatterMaterials;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import snownee.jade.addon.harvest.HarvestToolProvider;
+import snownee.jade.addon.harvest.SimpleToolHandler;
 import tesseract.Tesseract;
+
+import static muramasa.antimatter.data.AntimatterDefaultTools.WIRE_CUTTER;
+import static muramasa.antimatter.data.AntimatterDefaultTools.WRENCH;
 
 @WailaPlugin
 public class JadePlugin implements IWailaPlugin {
@@ -23,6 +30,8 @@ public class JadePlugin implements IWailaPlugin {
         registration.addConfig(HU, true);
         registration.addConfig(PROGRESS, true);
         registration.addConfig(STRUCTURE, true);
+        HarvestToolProvider.registerHandler(new SimpleToolHandler("wrench", WRENCH.getToolType(), WRENCH.getToolItem(AntimatterMaterials.Iron), WRENCH.getToolItem(AntimatterMaterials.Diamond), WRENCH.getToolItem(AntimatterMaterials.NetherizedDiamond)));
+        HarvestToolProvider.registerHandler(new SimpleToolHandler("wire_cutter", WIRE_CUTTER.getToolType(), WIRE_CUTTER.getToolItem(AntimatterMaterials.Iron), WIRE_CUTTER.getToolItem(AntimatterMaterials.Diamond), WIRE_CUTTER.getToolItem(AntimatterMaterials.NetherizedDiamond)));
     }
 
     @Override
