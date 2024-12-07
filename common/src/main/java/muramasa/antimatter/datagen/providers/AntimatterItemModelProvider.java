7aa6dc8ac180d442e8b05894ee8c0483a679f687
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2024-12-07 15:29:48 -0500

    fixed tools and armor generating models twice

--- common/src/main/java/muramasa/antimatter/datagen/providers/AntimatterItemModelProvider.java
+++ common/src/main/java/muramasa/antimatter/datagen/providers/AntimatterItemModelProvider.java
@@ -53,8 +53,6 @@ public class AntimatterItemModelProvider extends AntimatterModelProvider<Antimat
     public void processItemModels(String domain) {
         AntimatterAPI.all(Item.class, domain).forEach(i -> AntimatterModelManager.onItemModelBuild(i, this));
         AntimatterAPI.all(Block.class, domain).forEach(b -> AntimatterModelManager.onItemModelBuild(b, this));
-        AntimatterAPI.all(IAntimatterTool.class, domain).forEach(t -> tex(t.getItem(), "item/handheld", t.getTextures()));
-        AntimatterAPI.all(IAntimatterArmor.class, domain).forEach(t -> tex(t.getItem(), "item/handheld", t.getTextures()));
         AntimatterAPI.all(AntimatterFluid.class, domain).forEach(f -> {
             modelAndTexture(f.getContainerItem(), "forge", "item/bucket").bucketProperties(f.getFluid());
             modelAndTexture(f.getFluidBlock(), AntimatterBlockModelBuilder.getSimple()).tex(a -> a.put("all", f.getAttributes().flowing().toString()));
